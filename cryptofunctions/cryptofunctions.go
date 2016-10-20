c395ea5d2cebba1da6a4fc05cf45cea89aef5211
Satyam Sinha <satyam@aporeto.com>
2016-10-20 15:48:35 -0700

    New: crypto helper functions

---
+++ cryptofunctions/cryptofunctions.go
@@ -0,0 +1,158 @@
+package cryptofunctions
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/pem"
+	"fmt"
+)
+
+// ComputeHmac256 computes the HMAC256 of the message
+func ComputeHmac256(tags []byte, key []byte) []byte {
+
+	var buffer bytes.Buffer
+	binary.Write(&buffer, binary.BigEndian, tags)
+
+	h := hmac.New(sha256.New, key)
+
+	h.Write(buffer.Bytes())
+
+	return h.Sum(nil)
+
+}
+
+// VerifyHmac verifies if the HMAC of the message matches the one provided
+func VerifyHmac(tags []byte, expectedMAC []byte, key []byte) bool {
+
+	messageMAC := ComputeHmac256(tags, key)
+
+	return hmac.Equal(messageMAC, expectedMAC)
+}
+
+// GenerateRandomBytes returns securely generated random bytes.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// GenerateRandomString returns a URL-safe, base64 encoded
+// securely generated random string.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomString(s int) (string, error) {
+	b, err := GenerateRandomBytes(s)
+	return base64.URLEncoding.EncodeToString(b), err
+}
+
+// CreateEphemeralKey creates an ephmeral private/public key based on the
+// provided public key and the corresponding elliptic curve
+func CreateEphemeralKey(curve func() elliptic.Curve, pub *ecdsa.PublicKey) (*ecdsa.PrivateKey, []byte) {
+
+	ephemeral, err := ecdsa.GenerateKey(curve(), rand.Reader)
+	if err != nil {
+		return nil, []byte{}
+	}
+
+	ephPub := elliptic.Marshal(pub.Curve, ephemeral.PublicKey.X, ephemeral.PublicKey.Y)
+
+	return ephemeral, ephPub
+
+}
+
+// LoadRootCertificates loads the certificates in the provide PEM buffer in a CertPool
+func LoadRootCertificates(rootPEM []byte) *x509.CertPool {
+
+	roots := x509.NewCertPool()
+
+	ok := roots.AppendCertsFromPEM(rootPEM)
+	if !ok {
+		return nil
+	}
+
+	return roots
+
+}
+
+// LoadEllipticCurveKey parses and creates an EC key
+func LoadEllipticCurveKey(keyPEM []byte) (*ecdsa.PrivateKey, error) {
+	block, _ := pem.Decode(keyPEM)
+	if block == nil {
+		return nil, fmt.Errorf("Failed to Parse PEM block")
+	}
+
+	// Parse the key
+	key, err := x509.ParseECPrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+// LoadAndVerifyCertificate parses, validates, and creates a certificate structure from a PEM buffer
+// It must be provided with the a CertPool
+func LoadAndVerifyCertificate(certPEM []byte, roots *x509.CertPool) (*x509.Certificate, error) {
+
+	// Decode the certificate
+	certBlock, _ := pem.Decode(certPEM)
+	if certBlock == nil {
+		return nil, fmt.Errorf("Failed to decode PEM block")
+	}
+
+	// Create the certificate structure
+	cert, err := x509.ParseCertificate(certBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := x509.VerifyOptions{
+		Roots: roots,
+	}
+
+	if _, err := cert.Verify(opts); err != nil {
+		return nil, err
+	}
+
+	return cert, nil
+
+}
+
+// LoadAndVerifyECSecrets loads all the certificates and keys to memory in the right data structures
+func LoadAndVerifyECSecrets(keyPEM, certPEM, caCertPEM []byte) (key *ecdsa.PrivateKey, cert *x509.Certificate, rootCertPool *x509.CertPool, err error) {
+
+	// Parse the key
+	key, err = LoadEllipticCurveKey(keyPEM)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	rootCertPool = LoadRootCertificates(caCertPEM)
+	if rootCertPool == nil {
+		return nil, nil, nil, fmt.Errorf("Failed to load root certificate pool")
+	}
+
+	cert, err = LoadAndVerifyCertificate(certPEM, rootCertPool)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	return key, cert, rootCertPool, nil
+
+}
---
+++ cryptofunctions/cryptofunctions_test.go
@@ -0,0 +1,282 @@
+package cryptofunctions
+
+import (
+	"fmt"
+	"encoding/hex"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+// TestComputeVerifyHMAC tests the compute and verify of HMAC functions
+func TestComputeVerifyHMAC(t *testing.T) {
+	Convey("Given a token and a key", t, func() {
+		
+		fmt.Println("")
+		token := make([]byte, 256)
+		for i := uint8(0); i < 255; i++ {
+			token[i] = byte(i)
+		}
+		fmt.Println("Token:\n" + hex.Dump(token))
+
+		key := make([]byte, 32)
+		for i := uint8(0); i < 32; i++ {
+			key[i] = i
+		}
+		fmt.Println("Key:\n" + hex.Dump(key))
+
+		Convey("When I sign the token with the key", func() {
+			expectedMac := ComputeHmac256(token, key)
+			So(expectedMac, ShouldNotBeNil)
+
+			Convey("I should be able to verify the token with the same key", func() {
+				verified := VerifyHmac(token, expectedMac, key)
+				So(verified, ShouldBeTrue)
+			})
+
+			Convey("If I provide the worng key, I should fail verification", func() {
+				fakeKey := make([]byte, 32)
+				verified := VerifyHmac(token, expectedMac, fakeKey)
+				So(verified, ShouldBeFalse)
+			})
+
+			Convey("If I provide the wright key, but I havae the wrong signature, I should fail verification", func() {
+				failedMac := make([]byte, 32)
+				verified := VerifyHmac(token, failedMac, key)
+				So(verified, ShouldBeFalse)
+			})
+		})
+	})
+}
+
+// TestRandomString tests the random string generation function and the random byte generation
+func TestRandomString(t *testing.T) {
+	Convey("Given a string length of 16", t, func() {
+		length := 16
+		Convey("I should be able to generate two random strings that are not equal", func() {
+			string1, err1 := GenerateRandomString(length)
+			string2, err2 := GenerateRandomString(length)
+			So(err1, ShouldBeNil)
+			So(err2, ShouldBeNil)
+			So(string1, ShouldNotEqual, string2)
+		})
+	})
+	Convey("Given a string length of 32", t, func() {
+		length := 16
+		Convey("I should be able to generate two random strings that are not equal", func() {
+			string1, err1 := GenerateRandomString(length)
+			string2, err2 := GenerateRandomString(length)
+			So(err1, ShouldBeNil)
+			So(err2, ShouldBeNil)
+			So(string1, ShouldNotEqual, string2)
+		})
+	})
+}
+
+// TestFuncLoadEllipticCurve
+func TestFuncLoadEllipticCurve(t *testing.T) {
+	Convey("Given a valid EC key", t, func() {
+		keyPEM := `-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIPkiHqtH372JJdAG/IxJlE1gv03cdwa8Lhg2b3m/HmbyoAoGCCqGSM49
+AwEHoUQDQgAEAfAL+AfPj/DnxrU6tUkEyzEyCxnflOWxhouy1bdzhJ7vxMb1vQ31
+8ZbW/WvMN/ojIXqXYrEpISoojznj46w64w==
+-----END EC PRIVATE KEY-----`
+		Convey("I should be able to load the key", func() {
+			key, err := LoadEllipticCurveKey([]byte(keyPEM))
+			So(key, ShouldNotBeNil)
+			So(err, ShouldBeNil)
+		})
+	})
+
+	Convey("Given an invalid PEM BLOCK", t, func() {
+		keyPEM := ""
+		Convey("I should get an error", func() {
+			_, err := LoadEllipticCurveKey([]byte(keyPEM))
+			So(err, ShouldNotBeNil)
+		})
+	})
+
+	Convey("Given an invalid Key file", t, func() {
+		keyPEM := `-----BEGIN EC PRIVATE KEY-----
+-----END EC PRIVATE KEY-----`
+		Convey("I should get an error", func() {
+			_, err := LoadEllipticCurveKey([]byte(keyPEM))
+			So(err, ShouldNotBeNil)
+		})
+	})
+}
+
+// TestFuncLoadRootCertificates test the loading of root certs in a cert pool
+func TestFuncLoadRootCertificates(t *testing.T) {
+	Convey("Given a valid certificate chain", t, func() {
+		caPool := `-----BEGIN CERTIFICATE-----
+MIIBhTCCASwCCQC8b53yGlcQazAKBggqhkjOPQQDAjBLMQswCQYDVQQGEwJVUzEL
+MAkGA1UECAwCQ0ExDDAKBgNVBAcMA1NKQzEQMA4GA1UECgwHVHJpcmVtZTEPMA0G
+A1UEAwwGdWJ1bnR1MB4XDTE2MDkyNzIyNDkwMFoXDTI2MDkyNTIyNDkwMFowSzEL
+MAkGA1UEBhMCVVMxCzAJBgNVBAgMAkNBMQwwCgYDVQQHDANTSkMxEDAOBgNVBAoM
+B1RyaXJlbWUxDzANBgNVBAMMBnVidW50dTBZMBMGByqGSM49AgEGCCqGSM49AwEH
+A0IABJxneTUqhbtgEIwpKUUzwz3h92SqcOdIw3mfQkMjg3Vobvr6JKlpXYe9xhsN
+rygJmLhMAN9gjF9qM9ybdbe+m3owCgYIKoZIzj0EAwIDRwAwRAIgC1fVMqdBy/o3
+jNUje/Hx0fZF9VDyUK4ld+K/wF3QdK4CID1ONj/Kqinrq2OpjYdkgIjEPuXoOoR1
+tCym8dnq4wtH
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIB3jCCAYOgAwIBAgIJALsW7pyC2ERQMAoGCCqGSM49BAMCMEsxCzAJBgNVBAYT
+AlVTMQswCQYDVQQIDAJDQTEMMAoGA1UEBwwDU0pDMRAwDgYDVQQKDAdUcmlyZW1l
+MQ8wDQYDVQQDDAZ1YnVudHUwHhcNMTYwOTI3MjI0OTAwWhcNMjYwOTI1MjI0OTAw
+WjBLMQswCQYDVQQGEwJVUzELMAkGA1UECAwCQ0ExDDAKBgNVBAcMA1NKQzEQMA4G
+A1UECgwHVHJpcmVtZTEPMA0GA1UEAwwGdWJ1bnR1MFkwEwYHKoZIzj0CAQYIKoZI
+zj0DAQcDQgAE4c2Fd7XeIB1Vfs51fWwREfLLDa55J+NBalV12CH7YEAnEXjl47aV
+cmNqcAtdMUpf2oz9nFVI81bgO+OSudr3CqNQME4wHQYDVR0OBBYEFOBftuI09mmu
+rXjqDyIta1gT8lqvMB8GA1UdIwQYMBaAFOBftuI09mmurXjqDyIta1gT8lqvMAwG
+A1UdEwQFMAMBAf8wCgYIKoZIzj0EAwIDSQAwRgIhAMylAHhbFA0KqhXIFiXNpEbH
+JKaELL6UXXdeQ5yup8q+AiEAh5laB9rbgTymjaANcZ2YzEZH4VFS3CKoSdVqgnwC
+dW4=
+-----END CERTIFICATE-----`
+
+		Convey("I should be able to get a valid certificate chain", func() {
+			roots := LoadRootCertificates([]byte(caPool))
+			So(roots, ShouldNotBeNil)
+			So(len(roots.Subjects()), ShouldEqual, 2)
+		})
+	})
+}
+
+// TestFuncLoadAndVerifyCertificate
+func TestLoadAndVerifyCertificate(t *testing.T) {
+	Convey("Given a valid certificate chain", t, func() {
+		caPool := `-----BEGIN CERTIFICATE-----
+MIIBhTCCASwCCQC8b53yGlcQazAKBggqhkjOPQQDAjBLMQswCQYDVQQGEwJVUzEL
+MAkGA1UECAwCQ0ExDDAKBgNVBAcMA1NKQzEQMA4GA1UECgwHVHJpcmVtZTEPMA0G
+A1UEAwwGdWJ1bnR1MB4XDTE2MDkyNzIyNDkwMFoXDTI2MDkyNTIyNDkwMFowSzEL
+MAkGA1UEBhMCVVMxCzAJBgNVBAgMAkNBMQwwCgYDVQQHDANTSkMxEDAOBgNVBAoM
+B1RyaXJlbWUxDzANBgNVBAMMBnVidW50dTBZMBMGByqGSM49AgEGCCqGSM49AwEH
+A0IABJxneTUqhbtgEIwpKUUzwz3h92SqcOdIw3mfQkMjg3Vobvr6JKlpXYe9xhsN
+rygJmLhMAN9gjF9qM9ybdbe+m3owCgYIKoZIzj0EAwIDRwAwRAIgC1fVMqdBy/o3
+jNUje/Hx0fZF9VDyUK4ld+K/wF3QdK4CID1ONj/Kqinrq2OpjYdkgIjEPuXoOoR1
+tCym8dnq4wtH
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIB3jCCAYOgAwIBAgIJALsW7pyC2ERQMAoGCCqGSM49BAMCMEsxCzAJBgNVBAYT
+AlVTMQswCQYDVQQIDAJDQTEMMAoGA1UEBwwDU0pDMRAwDgYDVQQKDAdUcmlyZW1l
+MQ8wDQYDVQQDDAZ1YnVudHUwHhcNMTYwOTI3MjI0OTAwWhcNMjYwOTI1MjI0OTAw
+WjBLMQswCQYDVQQGEwJVUzELMAkGA1UECAwCQ0ExDDAKBgNVBAcMA1NKQzEQMA4G
+A1UECgwHVHJpcmVtZTEPMA0GA1UEAwwGdWJ1bnR1MFkwEwYHKoZIzj0CAQYIKoZI
+zj0DAQcDQgAE4c2Fd7XeIB1Vfs51fWwREfLLDa55J+NBalV12CH7YEAnEXjl47aV
+cmNqcAtdMUpf2oz9nFVI81bgO+OSudr3CqNQME4wHQYDVR0OBBYEFOBftuI09mmu
+rXjqDyIta1gT8lqvMB8GA1UdIwQYMBaAFOBftuI09mmurXjqDyIta1gT8lqvMAwG
+A1UdEwQFMAMBAf8wCgYIKoZIzj0EAwIDSQAwRgIhAMylAHhbFA0KqhXIFiXNpEbH
+JKaELL6UXXdeQ5yup8q+AiEAh5laB9rbgTymjaANcZ2YzEZH4VFS3CKoSdVqgnwC
+dW4=
+-----END CERTIFICATE-----`
+		roots := LoadRootCertificates([]byte(caPool))
+		So(roots, ShouldNotBeNil)
+		So(len(roots.Subjects()), ShouldEqual, 2)
+
+		Convey("Given a certificate signed by the intermediatery", func() {
+			certPEM := `-----BEGIN CERTIFICATE-----
+MIIBhjCCASwCCQCPCdgp39gHJTAKBggqhkjOPQQDAjBLMQswCQYDVQQGEwJVUzEL
+MAkGA1UECAwCQ0ExDDAKBgNVBAcMA1NKQzEQMA4GA1UECgwHVHJpcmVtZTEPMA0G
+A1UEAwwGdWJ1bnR1MB4XDTE2MDkyNzIyNDkwMFoXDTI2MDkyNTIyNDkwMFowSzEL
+MAkGA1UEBhMCVVMxCzAJBgNVBAgMAkNBMQwwCgYDVQQHDANTSkMxEDAOBgNVBAoM
+B1RyaXJlbWUxDzANBgNVBAMMBnVidW50dTBZMBMGByqGSM49AgEGCCqGSM49AwEH
+A0IABAHwC/gHz4/w58a1OrVJBMsxMgsZ35TlsYaLstW3c4Se78TG9b0N9fGW1v1r
+zDf6IyF6l2KxKSEqKI854+OsOuMwCgYIKoZIzj0EAwIDSAAwRQIgQwQn0jnK/XvD
+KxgQd/0pW5FOAaB41cMcw4/XVlphO1oCIQDlGie+WlOMjCzrV0Xz+XqIIi1pIgPT
+IG7Nv+YlTVp5qA==
+-----END CERTIFICATE-----`
+			Convey("I should be able to load and verify the certificate", func() {
+				cert, err := LoadAndVerifyCertificate([]byte(certPEM), roots)
+				So(cert, ShouldNotBeNil)
+				So(err, ShouldBeNil)
+			})
+		})
+	})
+
+	Convey("Given the root CA certificate only ", t, func() {
+		caPool := `
+-----BEGIN CERTIFICATE-----
+MIIB3jCCAYOgAwIBAgIJALsW7pyC2ERQMAoGCCqGSM49BAMCMEsxCzAJBgNVBAYT
+AlVTMQswCQYDVQQIDAJDQTEMMAoGA1UEBwwDU0pDMRAwDgYDVQQKDAdUcmlyZW1l
+MQ8wDQYDVQQDDAZ1YnVudHUwHhcNMTYwOTI3MjI0OTAwWhcNMjYwOTI1MjI0OTAw
+WjBLMQswCQYDVQQGEwJVUzELMAkGA1UECAwCQ0ExDDAKBgNVBAcMA1NKQzEQMA4G
+A1UECgwHVHJpcmVtZTEPMA0GA1UEAwwGdWJ1bnR1MFkwEwYHKoZIzj0CAQYIKoZI
+zj0DAQcDQgAE4c2Fd7XeIB1Vfs51fWwREfLLDa55J+NBalV12CH7YEAnEXjl47aV
+cmNqcAtdMUpf2oz9nFVI81bgO+OSudr3CqNQME4wHQYDVR0OBBYEFOBftuI09mmu
+rXjqDyIta1gT8lqvMB8GA1UdIwQYMBaAFOBftuI09mmurXjqDyIta1gT8lqvMAwG
+A1UdEwQFMAMBAf8wCgYIKoZIzj0EAwIDSQAwRgIhAMylAHhbFA0KqhXIFiXNpEbH
+JKaELL6UXXdeQ5yup8q+AiEAh5laB9rbgTymjaANcZ2YzEZH4VFS3CKoSdVqgnwC
+dW4=
+-----END CERTIFICATE-----`
+		roots := LoadRootCertificates([]byte(caPool))
+		So(roots, ShouldNotBeNil)
+		So(len(roots.Subjects()), ShouldEqual, 1)
+
+		Convey("Given a certificate signed by the intermediatery", func() {
+			certPEM := `-----BEGIN CERTIFICATE-----
+MIIBhjCCASwCCQCPCdgp39gHJTAKBggqhkjOPQQDAjBLMQswCQYDVQQGEwJVUzEL
+MAkGA1UECAwCQ0ExDDAKBgNVBAcMA1NKQzEQMA4GA1UECgwHVHJpcmVtZTEPMA0G
+A1UEAwwGdWJ1bnR1MB4XDTE2MDkyNzIyNDkwMFoXDTI2MDkyNTIyNDkwMFowSzEL
+MAkGA1UEBhMCVVMxCzAJBgNVBAgMAkNBMQwwCgYDVQQHDANTSkMxEDAOBgNVBAoM
+B1RyaXJlbWUxDzANBgNVBAMMBnVidW50dTBZMBMGByqGSM49AgEGCCqGSM49AwEH
+A0IABAHwC/gHz4/w58a1OrVJBMsxMgsZ35TlsYaLstW3c4Se78TG9b0N9fGW1v1r
+zDf6IyF6l2KxKSEqKI854+OsOuMwCgYIKoZIzj0EAwIDSAAwRQIgQwQn0jnK/XvD
+KxgQd/0pW5FOAaB41cMcw4/XVlphO1oCIQDlGie+WlOMjCzrV0Xz+XqIIi1pIgPT
+IG7Nv+YlTVp5qA==
+-----END CERTIFICATE-----`
+			Convey("I should be able to fail to verify the certificate ", func() {
+				cert, err := LoadAndVerifyCertificate([]byte(certPEM), roots)
+				So(cert, ShouldBeNil)
+				So(err, ShouldNotBeNil)
+			})
+		})
+	})
+
+	Convey("Given a good CA ", t, func() {
+		caPool := `-----BEGIN CERTIFICATE-----
+MIIBhTCCASwCCQC8b53yGlcQazAKBggqhkjOPQQDAjBLMQswCQYDVQQGEwJVUzEL
+MAkGA1UECAwCQ0ExDDAKBgNVBAcMA1NKQzEQMA4GA1UECgwHVHJpcmVtZTEPMA0G
+A1UEAwwGdWJ1bnR1MB4XDTE2MDkyNzIyNDkwMFoXDTI2MDkyNTIyNDkwMFowSzEL
+MAkGA1UEBhMCVVMxCzAJBgNVBAgMAkNBMQwwCgYDVQQHDANTSkMxEDAOBgNVBAoM
+B1RyaXJlbWUxDzANBgNVBAMMBnVidW50dTBZMBMGByqGSM49AgEGCCqGSM49AwEH
+A0IABJxneTUqhbtgEIwpKUUzwz3h92SqcOdIw3mfQkMjg3Vobvr6JKlpXYe9xhsN
+rygJmLhMAN9gjF9qM9ybdbe+m3owCgYIKoZIzj0EAwIDRwAwRAIgC1fVMqdBy/o3
+jNUje/Hx0fZF9VDyUK4ld+K/wF3QdK4CID1ONj/Kqinrq2OpjYdkgIjEPuXoOoR1
+tCym8dnq4wtH
+-----END CERTIFICATE-----`
+
+		roots := LoadRootCertificates([]byte(caPool))
+		So(roots, ShouldNotBeNil)
+		So(len(roots.Subjects()), ShouldEqual, 1)
+
+		Convey("Given a bad certificate ", func() {
+			certPEM := `-----BEGIN CERTIFICATE-----
+MAkGA1UECAwCQ0ExDDAKBgNVBAcMA1NKQzEQMA4GA1UECgwHVHJpcmVtZTEPMA0G
+A1UEAwwGdWJ1bnR1MB4XDTE2MDkyNzIyNDkwMFoXDTI2MDkyNTIyNDkwMFowSzEL
+MAkGA1UEBhMCVVMxCzAJBgNVBAgMAkNBMQwwCgYDVQQHDANTSkMxEDAOBgNVBAoM
+B1RyaXJlbWUxDzANBgNVBAMMBnVidW50dTBZMBMGByqGSM49AgEGCCqGSM49AwEH
+A0IABAHwC/gHz4/w58a1OrVJBMsxMgsZ35TlsYaLstW3c4Se78TG9b0N9fGW1v1r
+zDf6IyF6l2KxKSEqKI854+OsOuMwCgYIKoZIzj0EAwIDSAAwRQIgQwQn0jnK/XvD
+KxgQd/0pW5FOAaB41cMcw4/XVlphO1oCIQDlGie+WlOMjCzrV0Xz+XqIIi1pIgPT
+IG7Nv+YlTVp5qA==
+-----END CERTIFICATE-----`
+			Convey("I should be able to fail to verify the certificate ", func() {
+				cert, err := LoadAndVerifyCertificate([]byte(certPEM), roots)
+				So(cert, ShouldBeNil)
+				So(err, ShouldNotBeNil)
+			})
+		})
+	})
+
+	Convey("Given bad certificate   ", t, func() {
+
+		Convey("Where the certificate block is bad  ", func() {
+			certPEM := ``
+			Convey("I should be able to fail to verify the certificate ", func() {
+				cert, err := LoadAndVerifyCertificate([]byte(certPEM), nil)
+				So(cert, ShouldBeNil)
+				So(err, ShouldNotBeNil)
+			})
+		})
+	})
+}
