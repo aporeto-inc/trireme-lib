87ad27d871b21fc188706e15555b54179fa008dd
Amit Limaye <amit@aporeto.com>
2016-12-08 16:38:36 -0800

    RegisterName Fixes:Trials

--- enforcer/utils/rpc_payloads/rpc_handle.go
+++ enforcer/utils/rpc_payloads/rpc_handle.go
@@ -144,7 +144,7 @@ func (r *RPCWrapper) ProcessMessage(req *Request) bool {
 
 //RegisterTypes exported
 func RegisterTypes() {
-	gob.RegisterName("Init_Request_Payload", *(&InitRequestPayload{}))
+	gob.RegisterName("github.com/aporeto-inc/enforcer/utils/rpc_payloads/rpcWrapper.Init_Request_Payload", *(&InitRequestPayload{}))
 	gob.Register(InitResponsePayload{})
 	gob.Register(InitSupervisorPayload{})
 
