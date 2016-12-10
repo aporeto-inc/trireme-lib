d8a4864b546abc3cc9ea3b11ab7b637a8e0be0cd
Satyam Sinha <satyam@aporeto.com>
2016-12-09 17:56:31 -0800

    Fixed: Name of mock packages and some test cases

--- supervisor/iptablesutils/iptablesutils_test.go
+++ supervisor/iptablesutils/iptablesutils_test.go
@@ -1,9 +1,122 @@
-package iptablesutils_test
+package iptablesutils
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
+
+	"github.com/aporeto-inc/mock/gomock"
+	"github.com/aporeto-inc/trireme/supervisor/provider/mock"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestNothing(t *testing.T) {
+func TestAppChainPrefix(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
+	mock_ipt := mockprovider.NewMockIptablesProvider(ctrl)
+
+	Convey("Given I create an iptables utility", t, func() {
+
+		ipu := NewIptableUtils(mock_ipt)
+
+		Convey("When I call AppChainPrefix", func() {
+
+			context := "somecontext"
+			index := 345
+			prefix := ipu.AppChainPrefix(context, index)
+
+			Convey("Then I should get an AppChainPrefix", func() {
+
+				So(prefix, ShouldEqual, "TRIREME-App-"+context+"-"+strconv.Itoa(index))
+			})
+		})
+	})
+}
+
+func TestNetChainPrefix(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock_ipt := mockprovider.NewMockIptablesProvider(ctrl)
+
+	Convey("Given I create an iptables utility", t, func() {
+
+		ipu := NewIptableUtils(mock_ipt)
+
+		Convey("When I call NetChainPrefix", func() {
+
+			context := "somecontext"
+			index := 12321312
+			prefix := ipu.NetChainPrefix(context, index)
+
+			Convey("Then I should get an NetChainPrefix", func() {
+
+				So(prefix, ShouldEqual, "TRIREME-Net-"+context+"-"+strconv.Itoa(index))
+			})
+		})
+	})
+}
+
+func TestDefaultCacheIP(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock_ipt := mockprovider.NewMockIptablesProvider(ctrl)
+
+	Convey("Given I create an iptables utility", t, func() {
+
+		ipu := NewIptableUtils(mock_ipt)
+
+		Convey("When I call DefaultCacheIP with empty ip list", func() {
+
+			ip, err := ipu.DefaultCacheIP(nil)
+
+			Convey("Then I should get an error", func() {
+
+				So(ip, ShouldEqual, "")
+				So(err, ShouldNotBeNil)
+			})
+		})
+
+		Convey("When I call DefaultCacheIP with ip list", func() {
+
+			ips := []string{"172.0.0.1", "10.10.10.10"}
+			ip, err := ipu.DefaultCacheIP(ips)
+
+			Convey("Then I should get the first ip", func() {
+
+				So(ip, ShouldEqual, ips[0])
+				So(err, ShouldBeNil)
+			})
+		})
+	})
+}
+
+func TestFilterMarkedPacketsError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock_ipt := mockprovider.NewMockIptablesProvider(ctrl)
+
+	Convey("Given I create an iptables utility", t, func() {
+
+		ipu := NewIptableUtils(mock_ipt)
+
+		Convey("When I call FilterMarkedPackets to induce an error", func() {
+
+			mark := 10
+			mock_ipt.EXPECT().Insert(appAckPacketIPTableContext, appPacketIPTableSection, 1,
+				"-m", "mark",
+				"--mark", strconv.Itoa(mark),
+				"-j", "ACCEPT").Return(fmt.Errorf("Some Error"))
+
+			err := ipu.FilterMarkedPackets(mark)
+
+			Convey("Then I should get and error", func() {
+
+				So(err, ShouldNotBeNil)
+			})
+		})
+	})
 }
--- update_mocks.sh
+++ update_mocks.sh
@@ -4,16 +4,12 @@ go get github.com/aporeto-inc/mock/mockgen
 go get github.com/aporeto-inc/mock/gomock
 
 echo "Supervisor Mocks"
-mockgen -source supervisor/interfaces.go -destination supervisor/mock/mock_interfaces.go
-gofmt -w supervisor/mock/mock_interfaces.go
+mockgen -source supervisor/interfaces.go -destination supervisor/mock/mock_interfaces.go -package mockinterfaces 
 
-mockgen -source supervisor/iptablesutils/iptablesutils.go -destination supervisor/iptablesutils/mock/mock_iptablesutils.go
-gofmt -w supervisor/iptablesutils/mock/mock_iptablesutils.go
+mockgen -source supervisor/iptablesutils/iptablesutils.go -destination supervisor/iptablesutils/mock/mock_iptablesutils.go -package mockuptablesutils 
 
-mockgen -source supervisor/provider/ipsetprovider.go -destination supervisor/provider/mock/mock_ipsetprovider.go -self_package github.com/aporeto-inc/trireme/supervisor/provider
-gofmt -w supervisor/provider/mock/mock_ipsetprovider.go
+mockgen -source supervisor/provider/ipsetprovider.go -destination supervisor/provider/mock/mock_ipsetprovider.go -self_package github.com/aporeto-inc/trireme/supervisor/provider -package mockprovider 
 
-mockgen -source supervisor/provider/iptablesprovider.go -destination supervisor/provider/mock/mock_iptablesprovider.go
-gofmt -w supervisor/provider/mock/mock_iptablesprovider.go
+mockgen -source supervisor/provider/iptablesprovider.go -destination supervisor/provider/mock/mock_iptablesprovider.go -package mockprovider 
 
 echo >&2 "OK"
