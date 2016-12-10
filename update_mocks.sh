505201942bd4f185fa48572b8217bea92434c1e2
Satyam Sinha <satyam@aporeto.com>
2016-12-09 16:36:31 -0800

    Update update_mocks.sh to get latest mockgen;

--- update_mocks.sh
+++ update_mocks.sh
@@ -1,5 +1,8 @@
 #! /bin/bash -e
 
+go get github.com/aporeto-inc/mock/mockgen 
+go get github.com/aporeto-inc/mock/gomock
+
 echo "Supervisor Mocks"
 mockgen -source supervisor/interfaces.go -destination supervisor/mock/mock_interfaces.go
 gofmt -w supervisor/mock/mock_interfaces.go
