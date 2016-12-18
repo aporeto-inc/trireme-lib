b158915a0723bcad9bbc1e646633bfc70bec451e
Amit Limaye <amit@aporeto.com>
2016-12-18 14:38:30 -0800

    iptablesutils test passing

--- supervisor/iptablesutils/iptablesutils_test.go
+++ supervisor/iptablesutils/iptablesutils_test.go
@@ -19,7 +19,7 @@ func TestAppChainPrefix(t *testing.T) {
 
 	Convey("Given I create an iptables utility", t, func() {
 
-		ipu := NewIptableUtils(mock_ipt, false)
+		ipu := NewIptableUtils(mockIpt, false)
 
 		Convey("When I call AppChainPrefix", func() {
 
@@ -43,7 +43,7 @@ func TestNetChainPrefix(t *testing.T) {
 
 	Convey("Given I create an iptables utility", t, func() {
 
-		ipu := NewIptableUtils(mock_ipt, false)
+		ipu := NewIptableUtils(mockIpt, false)
 
 		Convey("When I call NetChainPrefix", func() {
 
@@ -67,7 +67,7 @@ func TestDefaultCacheIP(t *testing.T) {
 
 	Convey("Given I create an iptables utility", t, func() {
 
-		ipu := NewIptableUtils(mock_ipt, false)
+		ipu := NewIptableUtils(mockIpt, false)
 
 		Convey("When I call DefaultCacheIP with empty ip list", func() {
 
@@ -102,7 +102,7 @@ func TestFilterMarkedPacketsError(t *testing.T) {
 
 	Convey("Given I create an iptables utility", t, func() {
 
-		ipu := NewIptableUtils(mock_ipt, false)
+		ipu := NewIptableUtils(mockIpt, false)
 
 		Convey("When I call FilterMarkedPackets to induce an error", func() {
 
