58ecf95e162adb502d5dd6a531a3d2e466c6c120
Dimitri Stiliadis <dstiliadis@gmail.com>
2016-12-27 12:18:57 -0800

    Moving Swarm extractor to common

--- example/common/common.go
+++ example/common/common.go
@@ -1,7 +1,9 @@
 package common
 
 import (
+	"context"
 	"encoding/pem"
+	"fmt"
 	"io/ioutil"
 
 	"github.com/aporeto-inc/trireme"
@@ -9,6 +11,8 @@ import (
 	"github.com/aporeto-inc/trireme/monitor"
 	"github.com/aporeto-inc/trireme/policy"
 	"github.com/aporeto-inc/trireme/supervisor"
+	"github.com/docker/docker/api/types"
+	dockerClient "github.com/docker/docker/client"
 
 	log "github.com/Sirupsen/logrus"
 )
@@ -148,4 +152,55 @@ func TriremeWithPSK(networks []string, extractor *monitor.DockerMetadataExtracto
 
 	// Use this if you want a pre-shared key implementation
 	return configurator.NewPSKTriremeWithDockerMonitor("Server1", networks, policyEngine, nil, nil, false, []byte("THIS IS A BAD PASSWORD"), *extractor, remoteEnforcer)
+}
+
+// SwarmExtractor is an example metadata extractor for swarm that uses the service
+// labels for policy decisions
+func SwarmExtractor(info *types.ContainerJSON) (*policy.PURuntime, error) {
+
+	// Create a docker client
+	defaultHeaders := map[string]string{"User-Agent": "engine-api-dockerClient-1.0"}
+	cli, err := dockerClient.NewClient("unix:///var/run/docker.sock", "v1.23", nil, defaultHeaders)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"Package": "main",
+			"error":   err.Error(),
+		}).Debug("Failed to open docker connection")
+
+		return nil, fmt.Errorf("Error creating Docker Client %s", err)
+	}
+
+	// Get the labels from Docker. If it is a swarm service, get the labels from
+	// the service definition instead.
+	dockerLabels := info.Config.Labels
+	if _, ok := info.Config.Labels["com.docker.swarm.service.id"]; ok {
+
+		serviceID := info.Config.Labels["com.docker.swarm.service.id"]
+
+		service, _, err := cli.ServiceInspectWithRaw(context.Background(), serviceID)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"Package": "main",
+				"error":   err.Error(),
+			}).Debug("Failed get swarm labels")
+			return nil, fmt.Errorf("Error creating Docker Client %s", err)
+		}
+
+		dockerLabels = service.Spec.Labels
+	}
+
+	// Create the tags based on the docker labels
+	tags := policy.NewTagsMap(map[string]string{
+		"image": info.Config.Image,
+		"name":  info.Name,
+	})
+	for k, v := range dockerLabels {
+		tags.Add(k, v)
+	}
+
+	ipa := policy.NewIPMap(map[string]string{
+		"bridge": info.NetworkSettings.IPAddress,
+	})
+
+	return policy.NewPURuntime(info.Name, info.State.Pid, tags, ipa), nil
 }
