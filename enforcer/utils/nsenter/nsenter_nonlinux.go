064862d1dfb6cd78fb348ba8c0b98473d5706a1c
amitlimaye <amit.limaye@gmail.com>
2017-01-09 18:39:30 -0800

    namespace switching code not built for mac (#125)

---
+++ enforcer/utils/nsenter/nsenter_nonlinux.go
@@ -0,0 +1,7 @@
+// +build !linux
+//Package nsenter for switching namespaces
+package nsenter
+
+//This package should only run on linux
+//Don't test functionality of this package on non linux platforms as
+//the setns call is not available there
