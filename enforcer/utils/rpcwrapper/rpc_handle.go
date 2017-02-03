83ed338e6924ea197e729c36d9070797db4d8993
amitlimaye <amit.limaye@gmail.com>
2017-02-03 13:10:49 -0800

    Remote retries (#151)
    * Fix recovery from potential crash
    
    * Avoid error if the ns link is already there
    
    * Update command line args and fix paths
    
    * Update the example cli
    
    * FlowCache Fixes
    
    * RPC Retries is environment variable which can be tuned
    
    * Removed debug logs
    
    * Removed debug logs
    
    * Removed debug logs
    
    * Fixing the rpc sock path and flowcache

--- enforcer/utils/rpcwrapper/rpc_handle.go
+++ enforcer/utils/rpcwrapper/rpc_handle.go
@@ -12,6 +12,7 @@ import (
 	"net/rpc"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
 
 	"github.com/aporeto-inc/trireme/cache"
@@ -41,7 +42,8 @@ func NewRPCWrapper() *RPCWrapper {
 }
 
 const (
-	maxRetries = 100
+	maxRetries     = 1000
+	envRetryString = "REMOTE_RPCRETRIES"
 )
 
 //NewRPCClient exported
@@ -51,6 +53,14 @@ func (r *RPCWrapper) NewRPCClient(contextID string, channel string) error {
 
 	//establish new connection to context/container
 	RegisterTypes()
+	var max int
+	retries := os.Getenv(envRetryString)
+	if len(retries) > 0 {
+		max, _ = strconv.Atoi(retries)
+
+	} else {
+		max = maxRetries
+	}
 	numRetries := 0
 	client, err := rpc.DialHTTP("unix", channel)
 
@@ -58,7 +68,7 @@ func (r *RPCWrapper) NewRPCClient(contextID string, channel string) error {
 		time.Sleep(5 * time.Millisecond)
 
 		numRetries = numRetries + 1
-		if numRetries < maxRetries {
+		if numRetries < max {
 			client, err = rpc.DialHTTP("unix", channel)
 		} else {
 			return err
