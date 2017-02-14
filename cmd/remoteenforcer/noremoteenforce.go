89e9fce7c286a4d6ab560dfd734c43edc2682c4d
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-02-14 10:33:20 -0800

    fixed: build on darwin

---
+++ cmd/remoteenforcer/noremoteenforce.go
@@ -0,0 +1,22 @@
+// +build darwin
+
+package remoteenforcer
+
+import (
+	"github.com/aporeto-inc/trireme/enforcer"
+	"github.com/aporeto-inc/trireme/enforcer/utils/rpcwrapper"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+// Server is a fake implementation for building on darwin.
+type Server struct{}
+
+// EnforcerExit is a fake implementation for building on darwin.
+func (s *Server) EnforcerExit(req rpcwrapper.Request, resp *rpcwrapper.Response) error { return nil }
+
+// NewServer is a fake implementation for building on darwin.
+func NewServer(service enforcer.PacketProcessor, rpcchan string) *Server { return nil }
+
+// LaunchRemoteEnforcer is a fake implementation for building on darwin.
+func LaunchRemoteEnforcer(service enforcer.PacketProcessor, logLevel log.Level) {}
