15b929c8defdd5f895bb88d0c22c78a283e6753e
Amit Limaye <amit@aporeto.com>
2017-03-20 14:44:54 -0700

    Fixing unit cases

--- utils/extractor/external.go
+++ utils/extractor/external.go
@@ -57,5 +57,5 @@ func exampleExternalDockerMetadataExtractor(info *types.ContainerJSON) (*policy.
 	ipa := policy.NewIPMap(map[string]string{
 		"bridge": info.NetworkSettings.IPAddress,
 	})
-	return policy.NewPURuntime(info.Name, info.State.Pid, tagsMap, ipa, constants.ContainerPU, nil), nil
+	return policy.NewPURuntime(info.Name, info.NetworkSettings.SandboxKey, info.State.Pid, tagsMap, ipa, constants.ContainerPU, nil), nil
 }
