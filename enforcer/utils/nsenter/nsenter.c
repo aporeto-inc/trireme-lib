3cd08be2731e482d7f6b6592e49af7c0c3b4aeb7
Satyam Sinha <satyam@aporeto.com>
2017-04-12 19:55:13 -0700

    New: Added logs; converted path to stack variable and avoid calloc failure handling; error check on open by looking at fd

--- enforcer/utils/nsenter/nsenter.c
+++ enforcer/utils/nsenter/nsenter.c
@@ -9,22 +9,37 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include<errno.h>
-void nsexec(void){
-  char *path = NULL;
-  char *str = getenv("CONTAINER_PID");
-  int fd =0;
+#define STRBUF_SIZE     128
+void nsexec(void) {
+
+  int fd = 0;
+  char path[STRBUF_SIZE];
+  char msg[STRBUF_SIZE];
+  char * str = getenv("CONTAINER_PID");
   if(str == NULL){
-    //We are not running as remote enforcer
+    // We are not running as remote enforcer
+    setenv("NSENTER_LOGS", "no container pid", 1);
     return;
   }
-  int path_len = strlen("/proc/") + strlen(str) + strlen("/ns/net");
-  path = calloc(1,path_len+1);
-  snprintf(path,path_len+1,"/proc/%s/ns/net",str);
-  fd = open(path,O_RDONLY);
+
+  // Setup proc symlink
+  snprintf(path, sieof(path), "/proc/%s/ns/net", str);
+
+  // Setup FD to symlink
+  fd = open(path, O_RDONLY);
+  if(fd < 0) {
+    snprintf(msg, sizeof(msg), "path:%s fd:%d", path, fd)
+    setenv("NSENTER_ERROR_STATE",strerror(-ENOENT), 1);
+    setenv("NSENTER_LOGS", path, 1);
+    return;
+  }
+
+  // Set namespace
   int retval = setns(fd,0);
   if(retval < 0){
+    snprintf(msg, sizeof(msg), "path:%s fd:%d retval:%d", path, fd, retval)
     setenv("NSENTER_ERROR_STATE",strerror(errno),1);
+    setenv("NSENTER_LOGS",msg,1);
+    return;
   }
-  free(path);
-
 }
