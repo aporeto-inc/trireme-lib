0c8a3890ff72a82f870817b8847136c1ac7ca9f6
Satyam Sinha <satyam@aporeto.com>
2017-04-13 10:16:38 -0700

    Fixed: dont use ./... use glide novendor

--- .test.sh
+++ .test.sh
@@ -3,7 +3,7 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./...); do
+for d in $(go list $(glide novendor)); do
     go test -race -tags test -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
