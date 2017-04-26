7e0c6486193c32093ba445874d0e48d4b67804d1
Satyam Sinha <satyam@aporeto.com>
2017-04-25 22:39:16 -0700

    Fixed: move reporting to correct file

--- enforcer/datapath.go
+++ enforcer/datapath.go
@@ -168,6 +168,34 @@ func NewDefaultDatapathEnforcer(
 	)
 }
 
+func (d *datapathEnforcer) reportFlow(p *packet.Packet, connection *TCPConnection, sourceID string, destID string, context *PUContext, action string, mode string) {
+
+	if connection != nil {
+		connection.SetReported(true)
+	}
+	d.collector.CollectFlowEvent(&collector.FlowRecord{
+		ContextID:       context.ID,
+		DestinationID:   destID,
+		SourceID:        sourceID,
+		Tags:            context.Annotations,
+		Action:          action,
+		Mode:            mode,
+		SourceIP:        p.SourceAddress.String(),
+		DestinationIP:   p.DestinationAddress.String(),
+		DestinationPort: p.DestinationPort,
+	})
+}
+
+func (d *datapathEnforcer) reportAcceptedFlow(p *packet.Packet, connection *TCPConnection, sourceID string, destID string, context *PUContext) {
+
+	d.reportFlow(p, connection, sourceID, destID, context, collector.FlowAccept, "NA")
+}
+
+func (d *datapathEnforcer) reportRejectedFlow(p *packet.Packet, connection *TCPConnection, sourceID string, destID string, context *PUContext, mode string) {
+
+	d.reportFlow(p, connection, sourceID, destID, context, collector.FlowReject, mode)
+}
+
 func (d *datapathEnforcer) Enforce(contextID string, puInfo *policy.PUInfo) error {
 
 	hashSlice, err := d.contextTracker.Get(contextID)
--- enforcer/datapath_tcp.go
+++ enforcer/datapath_tcp.go
@@ -14,34 +14,6 @@ import (
 	"github.com/aporeto-inc/trireme/enforcer/utils/tokens"
 )
 
-func (d *datapathEnforcer) reportFlow(p *packet.Packet, connection *TCPConnection, sourceID string, destID string, context *PUContext, action string, mode string) {
-
-	if connection != nil {
-		connection.SetReported(true)
-	}
-	d.collector.CollectFlowEvent(&collector.FlowRecord{
-		ContextID:       context.ID,
-		DestinationID:   destID,
-		SourceID:        sourceID,
-		Tags:            context.Annotations,
-		Action:          action,
-		Mode:            mode,
-		SourceIP:        p.SourceAddress.String(),
-		DestinationIP:   p.DestinationAddress.String(),
-		DestinationPort: p.DestinationPort,
-	})
-}
-
-func (d *datapathEnforcer) reportAcceptedFlow(p *packet.Packet, connection *TCPConnection, sourceID string, destID string, context *PUContext) {
-
-	d.reportFlow(p, connection, sourceID, destID, context, collector.FlowAccept, "NA")
-}
-
-func (d *datapathEnforcer) reportRejectedFlow(p *packet.Packet, connection *TCPConnection, sourceID string, destID string, context *PUContext, mode string) {
-
-	d.reportFlow(p, connection, sourceID, destID, context, collector.FlowReject, mode)
-}
-
 // processNetworkPackets processes packets arriving from network and are destined to the application
 func (d *datapathEnforcer) processNetworkTCPPackets(p *packet.Packet) error {
 
