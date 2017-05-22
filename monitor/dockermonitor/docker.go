b03d6df6e338390557364bfa199f8584fb51b8ba
Bernard Van De Walle <bernard@bvandewa.com>
2017-05-22 15:13:14 -0700

    Adding info prints

--- monitor/dockermonitor/docker.go
+++ monitor/dockermonitor/docker.go
@@ -344,13 +344,16 @@ func (d *dockerMonitor) syncContainers() error {
 		container, err := d.dockerClient.ContainerInspect(context.Background(), c.ID)
 
 		if err != nil {
-			zap.L().Error("Error Syncing existing Container", zap.Error(err))
+			zap.L().Error("Error Syncing existing Container 1", zap.Error(err))
 			continue
 		}
 
 		if err := d.startDockerContainer(&container); err != nil {
-			zap.L().Error("Error Syncing existing Container", zap.Error(err))
+			zap.L().Error("Error Syncing existing Container 2", zap.Error(err))
 		}
+
+		zap.L().Info("Successfully synced container: %s", zap.String("ID", container.ID))
+
 	}
 
 	return nil
