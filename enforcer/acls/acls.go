02668b619d15f9ac693b134959be0980b110a733
Dimitri Stiliadis <dstiliadis@gmail.com>
2017-07-23 13:33:48 -0700

    Add the ACL logic

---
+++ enforcer/acls/acls.go
@@ -0,0 +1,171 @@
+package acls
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+
+	"github.com/aporeto-inc/trireme/policy"
+)
+
+// PortAction captures the minimum and maximum ports for an action
+type PortAction struct {
+	min    uint16
+	max    uint16
+	action policy.FlowAction
+}
+
+// PortActionList is a list of Port Actions
+type PortActionList []*PortAction
+
+// ACLCache holds all the ACLS in an internal DB
+// map[prefixes][subnets] -> list of ports with their actions
+type ACLCache struct {
+	prefixMap map[uint32]map[uint32]PortActionList
+}
+
+// NewACLCache creates a new ACL cache
+func NewACLCache() *ACLCache {
+	return &ACLCache{prefixMap: make(map[uint32]map[uint32]PortActionList)}
+}
+
+// createPortAction parses a port spec and creates the action
+func createPortAction(rule policy.IPRule) *PortAction {
+
+	p := &PortAction{}
+	if strings.Contains(rule.Port, ":") {
+		parts := strings.Split(rule.Port, ":")
+		if len(parts) != 2 {
+			return nil
+		}
+
+		port, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return nil
+		}
+		p.min = uint16(port)
+
+		port, err = strconv.Atoi(parts[1])
+		if err != nil {
+			return nil
+		}
+		p.max = uint16(port)
+
+	} else {
+		port, err := strconv.Atoi(rule.Port)
+		if err != nil {
+			return nil
+		}
+
+		p.min = uint16(port)
+
+		p.max = p.min
+	}
+
+	if p.min > p.max {
+		return nil
+	}
+
+	p.action = rule.Action
+
+	return p
+}
+
+// AddRule adds a single rule to the ACL Cache
+func (c *ACLCache) AddRule(rule policy.IPRule) (err error) {
+	var subnet, mask uint32
+
+	proto := strings.ToLower(rule.Protocol)
+
+	if proto != "tcp" {
+		return nil
+	}
+
+	parts := strings.Split(rule.Address, "/")
+
+	subnetSlice := net.ParseIP(parts[0])
+	if subnetSlice == nil {
+		return fmt.Errorf("Invalid address")
+	}
+
+	subnet = binary.BigEndian.Uint32(subnetSlice.To4())
+
+	switch len(parts) {
+	case 1:
+		mask = 0xFFFFFFFF
+	case 2:
+		maskvalue, err := strconv.Atoi(parts[1])
+		if err != nil || mask > 32 {
+			return fmt.Errorf("Invalid address")
+		}
+		mask = binary.BigEndian.Uint32(net.CIDRMask(maskvalue, 32))
+	default:
+		return fmt.Errorf("Invalid address")
+	}
+
+	if _, ok := c.prefixMap[mask]; !ok {
+		c.prefixMap[mask] = make(map[uint32]PortActionList)
+	}
+
+	a := createPortAction(rule)
+	if a == nil {
+		return fmt.Errorf("Invalid port")
+	}
+
+	fmt.Printf("Adding rule mask %x subnet %x for address %s \n ", mask, subnet, rule.Address)
+	c.prefixMap[mask][subnet] = append(c.prefixMap[mask][subnet], a)
+
+	return nil
+}
+
+// AddRuleList adds a list of rules to the cache
+func (c *ACLCache) AddRuleList(rules policy.IPRuleList) (err error) {
+
+	err = nil
+	for _, rule := range rules {
+		if lerr := c.AddRule(rule); err != nil {
+			err = lerr
+		}
+	}
+
+	fmt.Println("Print the whole ACLs table now ")
+	c.dumptable()
+	fmt.Println("Finished  ")
+
+	return err
+}
+
+// GetMatchingAction gets the matching action
+func (c *ACLCache) GetMatchingAction(ip []byte, port uint16) (policy.FlowAction, error) {
+	c.dumptable()
+	addr := binary.BigEndian.Uint32(ip)
+
+	fmt.Printf("Matching address %x and port %d \n", addr, port)
+
+	for bitmask, pmap := range c.prefixMap {
+		fmt.Printf("Iterating %x , target %x\n", bitmask, addr&bitmask)
+		if actionList, ok := pmap[addr&bitmask]; ok {
+			for _, p := range actionList {
+				if port >= p.min && port <= p.max {
+					fmt.Println("I Found a match ")
+					return p.action, nil
+				}
+			}
+		}
+	}
+
+	return policy.Reject, fmt.Errorf("No match")
+}
+
+func (c *ACLCache) dumptable() {
+	for bitmask, pmap := range c.prefixMap {
+		for actionlist, list := range pmap {
+			fmt.Printf("Bitmask %x actionlist %x  ", bitmask, actionlist)
+			for _, l := range list {
+				fmt.Println(l)
+			}
+		}
+	}
+}
---
+++ enforcer/acls/acls_test.go
@@ -0,0 +1,88 @@
+package acls
+
+import (
+	"net"
+	"testing"
+
+	"github.com/aporeto-inc/trireme/policy"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+var (
+	rules = policy.IPRuleList{
+		policy.IPRule{
+			Address: "172.17.0.0/16",
+			Port:    "400:500",
+			Action:  policy.Accept},
+		policy.IPRule{
+			Address: "192.168.100.0/24",
+			Port:    "80",
+			Action:  policy.Accept},
+		policy.IPRule{
+			Address: "10.1.1.1",
+			Port:    "80",
+			Action:  policy.Accept},
+		policy.IPRule{
+			Address: "0.0.0.0/0",
+			Port:    "443",
+			Action:  policy.Accept},
+	}
+)
+
+func TestLookup(t *testing.T) {
+
+	Convey("Given a good DB", t, func() {
+		c := NewACLCache()
+		err := c.AddRuleList(rules)
+		So(err, ShouldBeNil)
+		So(len(c.prefixMap), ShouldEqual, len(rules))
+
+		Convey("When I lookup for a matching address and a port range, I should get the right action", func() {
+			ip := net.ParseIP("172.17.0.1")
+			port := uint16(401)
+			a, err := c.GetMatchingAction(ip.To4(), port)
+			So(err, ShouldBeNil)
+			So(a, ShouldEqual, policy.Accept)
+		})
+
+		Convey("When I lookup for a matching address exact port, I should get the right action", func() {
+			ip := net.ParseIP("192.168.100.1")
+			port := uint16(80)
+			a, err := c.GetMatchingAction(ip.To4(), port)
+			So(err, ShouldBeNil)
+			So(a, ShouldEqual, policy.Accept)
+		})
+
+		Convey("When I lookup for a non matching address . I should get reject", func() {
+			ip := net.ParseIP("192.168.200.1")
+			port := uint16(80)
+			a, err := c.GetMatchingAction(ip.To4(), port)
+			So(err, ShouldNotBeNil)
+			So(a, ShouldEqual, policy.Reject)
+		})
+
+		Convey("When I lookup for a matching address but failed port, I should get reject", func() {
+			ip := net.ParseIP("192.168.100.1")
+			port := uint16(600)
+			a, err := c.GetMatchingAction(ip.To4(), port)
+			So(err, ShouldNotBeNil)
+			So(a, ShouldEqual, policy.Reject)
+		})
+
+		Convey("When I lookup for a matching exact address exact port, I should get the right action", func() {
+			ip := net.ParseIP("10.1.1.1")
+			port := uint16(80)
+			a, err := c.GetMatchingAction(ip.To4(), port)
+			So(err, ShouldBeNil)
+			So(a, ShouldEqual, policy.Accept)
+		})
+
+		Convey("When I lookup for a matching the 0.0.0.0/0 ACL, it shoud return succes ", func() {
+			ip := net.ParseIP("23.24.23.24")
+			port := uint16(443)
+			a, err := c.GetMatchingAction(ip.To4(), port)
+			So(err, ShouldBeNil)
+			So(a, ShouldEqual, policy.Accept)
+		})
+	})
+}
