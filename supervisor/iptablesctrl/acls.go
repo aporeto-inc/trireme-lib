032d606d022cbc5bc9cf8fe28ffd4bd2e8d4620a
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-07-29 10:50:40 -0700

    the fat fingers were somewhere else

--- supervisor/iptablesctrl/acls.go
+++ supervisor/iptablesctrl/acls.go
@@ -539,7 +539,7 @@ func (i *Instance) addNetACLs(contextID, chain, ip string, rules policy.IPRuleLi
 		"-s", "0.0.0.0/0",
 		"-m", "state", "--state", "NEW",
 		"-j", "NFLOG", "--nflog-group", "11",
-		"--nflog-prefix", contextID+":default:default:r",
+		"--nflog-prefix", contextID+":default:defaultr",
 	); err != nil {
 		return fmt.Errorf("Failed to add net log rule for table %s, chain %s, with %s", i.netPacketIPTableContext, chain, err.Error())
 	}
