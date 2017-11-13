cb583b3e3f750e9b4c8163bef941399373af0642
Satyam Sinha <satyam@aporeto.com>
2017-11-13 14:24:22 -0800

    Fixed: Moved processmon externally consumable API to processmon package

--- processmon/processmon.go
+++ processmon/processmon.go
@@ -1,11 +1,11 @@
 package processmon
 
-import "github.com/aporeto-inc/trireme/internal/processmon"
+import iprocessmon "github.com/aporeto-inc/trireme/internal/processmon"
 
 // SetupCommandArgs sets up arguments to be passed to the remote trireme instances
 func SetupCommandArgs(logToConsole bool, subProcessArgs []string) {
 
-	h := processmon.GetProcessManagerHdl()
+	h := iprocessmon.GetProcessManagerHdl()
 	if h == nil {
 		panic("Unable to find process manager handle")
 	}
