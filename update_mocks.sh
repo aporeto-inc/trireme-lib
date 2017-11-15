fc0ad689b199440966032341dbda546bccc91d71
Satyam Sinha <satyam@aporeto.com>
2017-11-14 17:24:04 -0800

    New: Add mocks for packages added

--- update_mocks.sh
+++ update_mocks.sh
@@ -6,8 +6,17 @@ go get github.com/golang/mock/gomock
 echo "Supervisor Mocks"
 mockgen -source supervisor/interfaces.go -destination supervisor/mock/mocksupervisor.go -package mocksupervisor
 
-echo "Processmon Mocks"
+echo "Internal/Processmon Mocks"
 mockgen -source internal/processmon/interfaces.go -destination internal/processmon/mock/mockprocessmon.go -package mockprocessmon
+
+echo "Internal/RemoteEnforcer Mocks"
+mockgen -source internal/remoteenforcer/interfaces.go -destination internal/remoteenforcer/mock/mockremoteenforcer.go -package mockremoteenforcer
+
+echo "Internal/RemoteEnforcer/StatsClient Mocks"
+mockgen -source internal/remoteenforcer/internal/statsclient/interfaces.go -destination internal/remoteenforcer/internal/statsclient/mock/mockstatsclient.go -package mockstatsclient
+
+echo "Internal/RemoteEnforcer/StatsCollector Mocks"
+mockgen -source internal/remoteenforcer/internal/statscollector/interfaces.go -aux_files collector=collector/interfaces.go -destination internal/remoteenforcer/internal/statscollector/mock/mockstatscollector.go -package mockstatscollector
 
 echo "Collector Mocks"
 mockgen -source collector/interfaces.go -destination collector/mock/mockcollector.go -package mockcollector -source_package github.com/aporeto-inc/trireme/collector
