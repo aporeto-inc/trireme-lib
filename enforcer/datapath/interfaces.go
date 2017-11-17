5eecd708952ff70c38394ee548b550b9dc99c702
Satyam Sinha <satyam@aporeto.com>
2017-11-16 19:53:32 -0800

    Fixed: package names and wrappers

--- enforcer/datapath/interfaces.go
+++ enforcer/datapath/interfaces.go
@@ -1,10 +1,12 @@
 package datapath
 
+// ContextProcessor is an interface to provide context checks
 type ContextProcessor interface {
 	DoesContextExist(contextID string) bool
 	IsContextServer(contextID string, backendip string) bool
 }
 
+// TokenProcessor is an interface to process tokens
 type TokenProcessor interface {
 	CreateSynToken(contextID string, payload []byte)
 	CreateSynAckToken(contextID string, payload []byte)
@@ -13,12 +15,15 @@ type TokenProcessor interface {
 	ParseAckToken(contextID string, data []byte)
 }
 
+// RuleProcessor is an interface to access rules
 type RuleProcessor interface {
 	CheckRejectRecvRules(contextID string) (int, bool)
 	CheckAcceptRecvRules(contextID string) (int, bool)
 	CheckRejectTxRules(contextID string) (int, bool)
 	CheckAcceptTxRules(contextID string) (int, bool)
 }
+
+// DatapathAccessor is an interface for datapth to access contexts/rules/tokens
 type DatapathAccessor interface {
 	ContextProcessor
 	TokenProcessor
