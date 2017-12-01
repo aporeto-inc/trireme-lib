d9c5c6afa29191e869b1904fad7d5a5ec390b492
Satyam Sinha <satyam@aporeto.com>
2017-12-01 00:04:21 -0800

    Intermediate checkin

---
+++ monitor/impl/uid/monitor.go
@@ -0,0 +1,114 @@
+package uidmonitor
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/aporeto-inc/trireme-lib/cache"
+	"github.com/aporeto-inc/trireme-lib/cgnetcls"
+	"github.com/aporeto-inc/trireme-lib/collector"
+	"github.com/aporeto-inc/trireme-lib/constants"
+	"github.com/aporeto-inc/trireme-lib/internal/contextstore"
+	"github.com/aporeto-inc/trireme-lib/monitor/impl"
+	"github.com/aporeto-inc/trireme-lib/monitor/rpc/events"
+	"github.com/aporeto-inc/trireme-lib/monitor/rpc/processor"
+)
+
+// UIDConfig is the configuration options to start a CNI monitor
+type UIDConfig struct {
+	EventMetadataExtractor events.EventMetadataExtractor
+	StorePath              string
+	ReleasePath            string
+}
+
+// uidMonitor captures all the monitor processor information for a UIDLoginPU
+// It implements the EventProcessor interface of the rpc monitor
+type uidMonitor struct {
+	proc *uidProcessor
+}
+
+// New returns a new implmentation of a monitor implmentation
+func New() monitorimpl.Implementation {
+
+	return &uidMonitor{
+		proc: &uidProcessor{},
+	}
+}
+
+// Start implements Implementation interface
+func (u *uidMonitor) Start() error {
+
+	if u.proc.collector == nil {
+		return fmt.Errorf("Missing configuration: collector")
+	}
+
+	if u.proc.syncHandler == nil {
+		return fmt.Errorf("Missing configuration: syncHandler")
+	}
+
+	if u.proc.puHandler == nil {
+		return fmt.Errorf("Missing configuration: puHandler")
+	}
+
+	return nil
+}
+
+// Stop implements Implementation interface
+func (u *uidMonitor) Stop() error {
+
+	return nil
+}
+
+// SetupConfig provides a configuration to implmentations. Every implmentation
+// can have its own config type.
+func (u *uidMonitor) SetupConfig(registerer processor.Registerer, cfg interface{}) error {
+
+	if cfg == nil {
+		cfg = &UIDConfig{}
+	}
+
+	uidConfig, ok := cfg.(UIDConfig)
+	if !ok {
+		return fmt.Errorf("Invalid configuration specified")
+	}
+
+	if registerer != nil {
+		registerer.RegisterProcessor(constants.UIDLoginPU, u.proc)
+	}
+
+	if uidConfig.ReleasePath == "" {
+		uidConfig.ReleasePath = "/var/lib/aporeto/cleaner"
+	}
+	u.proc.netcls = cgnetcls.NewCgroupNetController(uidConfig.ReleasePath)
+
+	if uidConfig.StorePath == "" {
+		uidConfig.StorePath = "/var/run/trireme/uid"
+	}
+	u.proc.contextStore = contextstore.NewFileContextStore(uidConfig.StorePath)
+	u.proc.storePath = uidConfig.StorePath
+
+	u.proc.regStart = regexp.MustCompile("^[a-zA-Z0-9_].{0,11}$")
+	u.proc.regStop = regexp.MustCompile("^/trireme/[a-zA-Z0-9_].{0,11}$")
+	u.proc.putoPidMap = cache.NewCache("putoPidMap")
+	u.proc.pidToPU = cache.NewCache("pidToPU")
+
+	if uidConfig.EventMetadataExtractor == nil {
+		uidConfig.EventMetadataExtractor = MetadataExtractor
+	}
+	u.proc.metadataExtractor = uidConfig.EventMetadataExtractor
+	if u.proc.metadataExtractor == nil {
+		return fmt.Errorf("Unable to setup a metadata extractor")
+	}
+
+	return nil
+}
+
+// SetupHandlers sets up handlers for monitors to invoke for various events such as
+// processing unit events and synchronization events. This will be called before Start()
+// by the consumer of the monitor
+func (u *uidMonitor) SetupHandlers(collector collector.EventCollector, puHandler monitorimpl.ProcessingUnitsHandler, syncHandler monitorimpl.SynchronizationHandler) {
+
+	u.proc.collector = collector
+	u.proc.puHandler = puHandler
+	u.proc.syncHandler = syncHandler
+}
