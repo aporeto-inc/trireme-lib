e0d7466a465ebe0cec5a6095dc7d80d39eb9f828
Dimitri Stiliadis <dstiliadis@users.noreply.github.com>
2017-12-04 11:53:09 -0800

    Service context for claims  (#378)
    Ephemeral keys for connections

--- enforcer/datapath/tokenaccessor/tokenaccessor.go
+++ enforcer/datapath/tokenaccessor/tokenaccessor.go
@@ -90,13 +90,15 @@ func (t *tokenAccessor) CreateSynPacketToken(context *pucontext.PUContext, auth 
 		// Randomize the nonce and send it
 		auth.LocalContext, err = t.getToken().Randomize(context.SynToken)
 		if err == nil {
+			auth.LocalServiceContext = context.SynServiceContext
 			return context.SynToken, nil
 		}
 		// If there is an error, let's try to create a new one
 	}
 
 	claims := &tokens.ConnectionClaims{
-		T: context.Identity,
+		T:  context.Identity,
+		EK: auth.LocalServiceContext,
 	}
 
 	if context.SynToken, auth.LocalContext, err = t.getToken().CreateAndSign(false, claims); err != nil {
@@ -104,6 +106,7 @@ func (t *tokenAccessor) CreateSynPacketToken(context *pucontext.PUContext, auth 
 	}
 
 	context.SynExpiration = time.Now().Add(time.Millisecond * 500)
+	context.SynServiceContext = auth.LocalServiceContext
 
 	return context.SynToken, nil
 }
@@ -115,13 +118,14 @@ func (t *tokenAccessor) CreateSynAckPacketToken(context *pucontext.PUContext, au
 	claims := &tokens.ConnectionClaims{
 		T:   context.Identity,
 		RMT: auth.RemoteContext,
+		EK:  auth.LocalServiceContext,
 	}
 
-	if context.SynToken, auth.LocalContext, err = t.getToken().CreateAndSign(false, claims); err != nil {
+	if token, auth.LocalContext, err = t.getToken().CreateAndSign(false, claims); err != nil {
 		return []byte{}, nil
 	}
 
-	return context.SynToken, nil
+	return token, nil
 }
 
 // parsePacketToken parses the packet token and populates the right state.
@@ -143,6 +147,7 @@ func (t *tokenAccessor) ParsePacketToken(auth *connection.AuthInfo, data []byte)
 	auth.RemotePublicKey = cert
 	auth.RemoteContext = nonce
 	auth.RemoteContextID = remoteContextID
+	auth.RemoteServiceContext = claims.EK
 
 	return claims, nil
 }
