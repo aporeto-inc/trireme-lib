683f93cdca890f70b9c356e097194b05c2d55bb4
Bernard Van De Walle <bernard@bvandewa.com>
2017-12-22 10:53:48 -0800

    New: Adding Types for SubMonitor options (#438)
    * New: Adding Types for SubMonitor options
    
    * Adding monitor

--- monitor.go
+++ monitor.go
@@ -14,6 +14,18 @@ import (
 // MonitorOption is provided using functional arguments.
 type MonitorOption func(*monitor.Config)
 
+// CNIMonitorOption is provided using functional arguments.
+type CNIMonitorOption func(*cnimonitor.Config)
+
+// UIDMonitorOption is provided using functional arguments.
+type UIDMonitorOption func(*uidmonitor.Config)
+
+// DockerMonitorOption is provided using functional arguments.
+type DockerMonitorOption func(*dockermonitor.Config)
+
+// LinuxMonitorOption is provided using functional arguments.
+type LinuxMonitorOption func(*linuxmonitor.Config)
+
 // SubOptionMonitorLinuxExtractor provides a way to specify metadata extractor for linux monitors.
 func SubOptionMonitorLinuxExtractor(extractor events.EventMetadataExtractor) func(*linuxmonitor.Config) {
 	return func(cfg *linuxmonitor.Config) {
