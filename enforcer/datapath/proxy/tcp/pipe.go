304944b3afca8cd8724f882bef1a334adce07a83
Amit Limaye <amit@aporeto.com>
2018-01-19 13:40:08 -0800

    WIP

--- enforcer/datapath/proxy/tcp/pipe.go
+++ enforcer/datapath/proxy/tcp/pipe.go
@@ -118,6 +118,62 @@ func CopyPipe(a, b net.Conn) error {
 	return nil
 }
 
-func copyEncryptedFlow(tlsConn *tls.Conn, fd int) {
+func copyEncryptedFlow(tlsConn *tls.Conn, fd int) error {
+	defer func() {
+		if err := tlsConn.Close(); err != nil {
+			zap.L().Error("Failed to close TLS Connection", zap.Error(err))
+		}
+		if err := syscall.Close(fd); err != nil {
+			zap.L().Error("Cannot Close socket to application", zap.Error(err))
+		}
+	}()
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		readSlice := make([]byte, 1024)
+		//from tlsConn --> fd
 
+		for {
+			if n, err := tlsConn.Read(readSlice); err == nil {
+				if n > 0 {
+					if n, err = syscall.Write(fd, readSlice[:n]); err != nil {
+						continue
+					}
+				}
+
+			}
+
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		//fd --> tlsConn
+		readSlice := make([]byte, 1024)
+		events := make([]syscall.EpollEvent, 1)
+		if err := syscall.SetNonblock(fd, true); err != nil {
+			zap.L().Error("Cannot put socket in non-blocking mode")
+		}
+		var epfd int
+		var epollerr error
+		if epfd, epollerr = syscall.EpollCreate1(0); epollerr != nil {
+			zap.L().Error("Unable to create epoll set")
+			return
+		}
+		defer syscall.Close(epfd) // nolint
+		for {
+			if err := syscall.EpollCtl(epfd, syscall.EPOLL_CTL_ADD, fd, &syscall.EpollEvent{Events: syscall.EPOLLIN, Fd: int32(fd)}); err != nil {
+				zap.L().Error("Cannot add socket to epoll wait", zap.Error(err))
+			}
+			n, err := syscall.EpollWait(epfd, events, 10)
+			if n, err := syscall.Read(fd, readSlice); err == nil {
+				if n, err = tlsConn.Write(readSlice[:n]); err != nil {
+					zap.L().Debug("Could Not Write Data to remote")
+				}
+			}
+		}
+	}()
+	wg.Wait()
+	return nil
 }
