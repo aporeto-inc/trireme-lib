16b3890071f3cf7e19c7be296ef277b44b1b405f
Dimitri Stiliadis <dstiliadis@gmail.com>
2018-03-06 14:30:18 -0800

    Lock around iptables access by a supervisor

--- controller/internal/supervisor/supervisor.go
+++ controller/internal/supervisor/supervisor.go
@@ -142,6 +142,9 @@ func (s *Config) Run(ctx context.Context) error {
 
 // CleanUp implements the cleanup interface
 func (s *Config) CleanUp() error {
+	s.Lock()
+	defer s.Unlock()
+
 	return s.impl.CleanUp()
 }
 
@@ -161,6 +164,9 @@ func (s *Config) SetTargetNetworks(networks []string) error {
 }
 
 func (s *Config) doCreatePU(contextID string, pu *policy.PUInfo) error {
+
+	s.Lock()
+	defer s.Unlock()
 
 	c := &cacheData{
 		version:       0,
@@ -187,6 +193,9 @@ func (s *Config) doCreatePU(contextID string, pu *policy.PUInfo) error {
 // UpdatePU creates a mapping between an IP address and the corresponding labels
 //and the invokes the various handlers that process all policies.
 func (s *Config) doUpdatePU(contextID string, pu *policy.PUInfo) error {
+
+	s.Lock()
+	defer s.Unlock()
 
 	data, err := s.versionTracker.LockedModify(contextID, revert, 1)
 	if err != nil {
