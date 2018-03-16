b2c3ec0e2eadfd20364827d2694294dd32778761
Bernard Van De Walle <bernard@bvandewa.com>
2018-03-16 11:36:07 +0100

    Adding Kubernetes logic

--- monitor/internal/kubernetes/handler.go
+++ monitor/internal/kubernetes/handler.go
@@ -9,6 +9,7 @@ import (
 
 // HandlePUEvent is called by all monitors when a PU event is generated. The implementer
 // is responsible to update all components by explicitly adding a new PU.
+// Specifically for Kubernetes, The monitor
 func (m *KubernetesMonitor) HandlePUEvent(ctx context.Context, puID string, event common.Event, runtime policy.RuntimeReader) error {
 	return nil
 }
