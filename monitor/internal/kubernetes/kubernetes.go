f9750590f00eb90c6053fb46cfe252f4a051631a
Bernard Van De Walle <bernard@bvandewa.com>
2018-03-21 17:19:50 +0100

    Rewriting Tafgs logic

--- monitor/internal/kubernetes/kubernetes.go
+++ monitor/internal/kubernetes/kubernetes.go
@@ -1,6 +1,8 @@
 package kubernetesmonitor
 
 import (
+	"fmt"
+
 	"github.com/aporeto-inc/trireme-lib/policy"
 )
 
@@ -16,8 +18,58 @@ const KubernetesContainerNameIdentifier = "@usr:io.kubernetes.container.name"
 // KubernetesInfraContainerName is the name of the infra POD.
 const KubernetesInfraContainerName = "POD"
 
-func (m *KubernetesMonitor) consolidateKubernetesTags(runtime policy.RuntimeReader) (policy.RuntimeReader, error) {
-	return runtime, nil
+// UpstreamNameIdentifier is the identifier used to identify the nane on the resulting PU
+const UpstreamNameIdentifier = "k8s:name"
+
+// UpstreamNamespaceIdentifier is the identifier used to identify the nanespace on the resulting PU
+const UpstreamNamespaceIdentifier = "k8s:namespace"
+
+func (m *KubernetesMonitor) consolidateKubernetesTags(runtime policy.RuntimeReader) (*policy.PURuntime, error) {
+
+	podName, ok := runtime.Tag(KubernetesPodNameIdentifier)
+	if !ok {
+		return nil, fmt.Errorf("Error getting Kubernetes Pod name")
+	}
+	podNamespace, ok := runtime.Tag(KubernetesPodNamespaceIdentifier)
+	if !ok {
+		return nil, fmt.Errorf("Error getting Kubernetes Pod namespace")
+	}
+
+	pod, err := m.kubernetesClient.Pod(podName, podNamespace)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't get labels for pod %s : %v", podName, err)
+	}
+
+	// If IP is empty, wait for an UpdatePodEvent with the Actual PodIP. Not ready to be activated now.
+	if pod.Status.PodIP == "" {
+		return nil, nil
+	}
+	// If Pod is running in the hostNS, no activation (not supported).
+	if pod.Status.PodIP == pod.Status.HostIP {
+		return nil, nil
+	}
+
+	podLabels := pod.GetLabels()
+	if podLabels == nil {
+		return nil, nil
+	}
+	fmt.Printf("\n\n Tags before: %v \n\n", runtime.Tags())
+
+	tags := policy.NewTagStoreFromMap(podLabels)
+	tags.AppendKeyValue(UpstreamNameIdentifier, podName)
+	tags.AppendKeyValue(UpstreamNamespaceIdentifier, podNamespace)
+
+	originalRuntime, ok := runtime.(*policy.PURuntime)
+	if !ok {
+		return nil, fmt.Errorf("Error casting puruntime")
+	}
+
+	newRuntime := originalRuntime.Clone()
+	newRuntime.SetTags(tags)
+
+	fmt.Printf("\n\n Tags after: %v \n\n", newRuntime.Tags())
+
+	return newRuntime, nil
 }
 
 // isPodInfraContainer returns true if the runtime represents the infra container for the POD
