e9e7b3b7ebe08206c3b3b851fa5bd6c3ba97f55c
Bernard Van De Walle <bernard@bvandewa.com>
2018-04-13 09:20:00 -0700

    Adding squeleton for more handler tests

--- monitor/internal/kubernetes/handler_test.go
+++ monitor/internal/kubernetes/handler_test.go
@@ -1,9 +1,16 @@
 package kubernetesmonitor
 
 import (
+	"context"
 	"testing"
 
+	kubernetesclient "github.com/aporeto-inc/trireme-kubernetes/kubernetes"
+	"github.com/aporeto-inc/trireme-lib/common"
+	"github.com/aporeto-inc/trireme-lib/monitor/config"
+	"github.com/aporeto-inc/trireme-lib/monitor/extractors"
+	dockermonitor "github.com/aporeto-inc/trireme-lib/monitor/internal/docker"
 	"github.com/aporeto-inc/trireme-lib/policy"
+	kubecache "k8s.io/client-go/tools/cache"
 )
 
 func Test_getKubernetesInformation(t *testing.T) {
@@ -102,6 +109,69 @@ func Test_getKubernetesInformation(t *testing.T) {
 			}
 			if got1 != tt.want1 {
 				t.Errorf("getKubernetesInformation() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func TestKubernetesMonitor_HandlePUEvent(t *testing.T) {
+	type fields struct {
+		dockerMonitor       *dockermonitor.DockerMonitor
+		kubernetesClient    *kubernetesclient.Client
+		handlers            *config.ProcessorConfig
+		cache               *cache
+		kubernetesExtractor extractors.KubernetesMetadataExtractorType
+		podStore            kubecache.Store
+		podController       kubecache.Controller
+		podControllerStop   chan struct{}
+		enableHostPods      bool
+	}
+	type args struct {
+		ctx           context.Context
+		puID          string
+		event         common.Event
+		dockerRuntime policy.RuntimeReader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:   "empty dockerruntime on create",
+			fields: fields{},
+			args: args{
+				event:         common.EventCreate,
+				dockerRuntime: policy.NewPURuntimeWithDefaults(),
+			},
+			wantErr: true,
+		},
+		{
+			name:   "empty dockerruntime on start",
+			fields: fields{},
+			args: args{
+				event:         common.EventCreate,
+				dockerRuntime: policy.NewPURuntimeWithDefaults(),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &KubernetesMonitor{
+				dockerMonitor:       tt.fields.dockerMonitor,
+				kubernetesClient:    tt.fields.kubernetesClient,
+				handlers:            tt.fields.handlers,
+				cache:               tt.fields.cache,
+				kubernetesExtractor: tt.fields.kubernetesExtractor,
+				podStore:            tt.fields.podStore,
+				podController:       tt.fields.podController,
+				podControllerStop:   tt.fields.podControllerStop,
+				enableHostPods:      tt.fields.enableHostPods,
+			}
+			if err := m.HandlePUEvent(tt.args.ctx, tt.args.puID, tt.args.event, tt.args.dockerRuntime); (err != nil) != tt.wantErr {
+				t.Errorf("KubernetesMonitor.HandlePUEvent() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
