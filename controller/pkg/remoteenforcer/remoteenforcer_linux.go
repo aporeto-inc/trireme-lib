bdd6bf41e427567c1a70278221e1e36861646fb2
Varun Kulkarni <var.kulkarni@gmail.com>
2018-04-23 09:47:59 -0700

    remove extra error statement

--- controller/pkg/remoteenforcer/remoteenforcer_linux.go
+++ controller/pkg/remoteenforcer/remoteenforcer_linux.go
@@ -369,7 +369,6 @@ func (s *RemoteEnforcer) UpdateSecrets(req rpcwrapper.Request, resp *rpcwrapper.
 	cmdLock.Lock()
 	defer cmdLock.Unlock()
 	if s.enforcer == nil {
-		zap.L().Error(resp.Status)
 		return fmt.Errorf(resp.Status)
 	}
 
