6265926f6eaf31b2ffe223e847c374850328b918
Amit Limaye <amit@aporeto.com>
2018-05-24 16:57:25 -0700

    Handling for port ranges in ipset

--- controller/internal/supervisor/iptablesctrl/iptables.go
+++ controller/internal/supervisor/iptablesctrl/iptables.go
@@ -8,7 +8,8 @@ import (
 	"fmt"
 	"io"
 	"strconv"
-
+	"strings"
+	
 	"github.com/aporeto-inc/trireme-lib/common"
 	"github.com/aporeto-inc/trireme-lib/controller/constants"
 	"github.com/aporeto-inc/trireme-lib/controller/internal/portset"
@@ -381,7 +382,7 @@ func (i *Instance) configureLinuxRules(contextID, appChain, netChain, proxyPortS
 		}
 	}
 
-	i.addPortToListenerPortSet(port) //nolint
+	i.addPortToListenerPortSet(strings.Replace(port,":","-",-1)) //nolint
 	return i.addChainRules(portSetName, appChain, netChain, port, mark, uid, proxyPort, proxyPortSetName)
 }
 
