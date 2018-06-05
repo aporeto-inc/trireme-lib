b1aeec0fea514462bb01f0220cb72984af017c65
Dimitri Stiliadis <dstiliadis@users.noreply.github.com>
2018-06-05 11:59:46 -0400

    Improvements in URI search  (#532)

--- controller/pkg/urisearch/urisearch.go
+++ controller/pkg/urisearch/urisearch.go
@@ -1,6 +1,8 @@
 package urisearch
 
 import (
+	"fmt"
+
 	"github.com/aporeto-inc/trireme-lib/policy"
 )
 
@@ -17,6 +19,11 @@ type APICache struct {
 	External    bool
 }
 
+type scopeRule struct {
+	rule   *policy.HTTPRule
+	scopes map[string]struct{}
+}
+
 // NewAPICache creates a new API cache
 func NewAPICache(rules []*policy.HTTPRule, id string, external bool) *APICache {
 	a := &APICache{
@@ -26,17 +33,58 @@ func NewAPICache(rules []*policy.HTTPRule, id string, external bool) *APICache {
 	}
 
 	for _, rule := range rules {
+		sc := &scopeRule{
+			rule:   rule,
+			scopes: map[string]struct{}{},
+		}
+		for _, term := range rule.Scopes {
+			sc.scopes[term] = struct{}{}
+		}
 		for _, method := range rule.Methods {
 			if _, ok := a.methodRoots[method]; !ok {
 				a.methodRoots[method] = &node{}
 			}
 			for _, uri := range rule.URIs {
-				insert(a.methodRoots[method], uri, rule)
+				insert(a.methodRoots[method], uri, sc)
 			}
 		}
 	}
 
 	return a
+}
+
+// FindRule finds a rule in the APICache without validating scopes
+func (c *APICache) FindRule(verb, uri string) (bool, *policy.HTTPRule) {
+	found, rule := c.Find(verb, uri)
+	if rule == nil {
+		return found, nil
+	}
+	if policyRule, ok := rule.(*scopeRule); ok {
+		return found, policyRule.rule
+	}
+	return false, nil
+}
+
+// FindAndMatchScope finds the rule and returns true only if the scope matches
+// as well.
+func (c *APICache) FindAndMatchScope(verb, uri string, attributes []string) bool {
+	found, rule := c.Find(verb, uri)
+	if !found || rule == nil {
+		return false
+	}
+	policyRule, ok := rule.(*scopeRule)
+	if !ok {
+		return false
+	}
+	if policyRule.rule.Public {
+		return true
+	}
+	for _, attr := range attributes {
+		if _, ok := policyRule.scopes[attr]; ok {
+			return true
+		}
+	}
+	return false
 }
 
 // Find finds a URI in the cache and returns true and the data if found.
@@ -46,7 +94,6 @@ func (c *APICache) Find(verb, uri string) (bool, interface{}) {
 	if !ok {
 		return false, nil
 	}
-
 	return search(root, uri)
 }
 
@@ -96,6 +143,7 @@ func insert(n *node, api string, data interface{}) {
 }
 
 func search(n *node, api string) (found bool, data interface{}) {
+
 	prefix, suffix := parse(api)
 
 	if prefix == "/" {
@@ -105,14 +153,39 @@ func search(n *node, api string) (found bool, data interface{}) {
 	}
 
 	next, foundPrefix := n.children[prefix]
-	if !foundPrefix {
-		// If not found, try the star
-		next, foundPrefix = n.children["/*"]
-	}
-
 	// We found either an exact match or a * match
 	if foundPrefix {
-		return search(next, suffix)
+		matchedChildren, data := search(next, suffix)
+		if matchedChildren {
+			return true, data
+		}
+	}
+
+	// If not found, try the ignore operator.
+	next, foundPrefix = n.children["/?"]
+	if foundPrefix {
+		matchedChildren, data := search(next, suffix)
+		if matchedChildren {
+			return true, data
+		}
+	}
+
+	// If not found, try the * operator and ignore the rest of path.
+	next, foundPrefix = n.children["/*"]
+	if foundPrefix {
+		for len(suffix) > 0 {
+			fmt.Println("Testing with suffix ", suffix)
+			matchedChildren, data := search(next, suffix)
+			if matchedChildren {
+				return true, data
+			}
+			prefix, suffix = parse(suffix)
+			fmt.Println("New suffix ", suffix)
+		}
+		matchedChildren, data := search(next, "/")
+		if matchedChildren {
+			return true, data
+		}
 	}
 
 	if n.leaf && len(prefix) == 0 {
