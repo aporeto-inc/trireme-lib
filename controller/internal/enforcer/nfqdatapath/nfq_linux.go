14ffd4a3befb3155d7f7719ac40169d369d6b81f
Varun Kulkarni <var.kulkarni@gmail.com>
2018-06-28 12:59:02 -0700

    disable udp for now

--- controller/internal/enforcer/nfqdatapath/nfq_linux.go
+++ controller/internal/enforcer/nfqdatapath/nfq_linux.go
@@ -81,7 +81,11 @@ func (d *Datapath) processNetworkPacketsFromNFQ(p *nfqueue.NFPacket) {
 	} else if netPacket.IPProto == packet.IPProtocolTCP {
 		err = d.processNetworkTCPPackets(netPacket)
 	} else if netPacket.IPProto == packet.IPProtocolUDP {
-		err = d.ProcessNetworkUDPPacket(netPacket)
+		length := uint32(len(p.Buffer))
+		buffer := p.Buffer
+		p.QueueHandle.SetVerdict2(uint32(p.QueueHandle.QueueNum), 1, uint32(p.Mark), length, uint32(p.ID), buffer)
+		return
+		//err = d.ProcessNetworkUDPPacket(netPacket)
 	} else {
 		err = fmt.Errorf("invalid ip protocol: %d", netPacket.IPProto)
 	}
@@ -124,7 +128,12 @@ func (d *Datapath) processApplicationPacketsFromNFQ(p *nfqueue.NFPacket) {
 	} else if appPacket.IPProto == packet.IPProtocolTCP {
 		err = d.processApplicationTCPPackets(appPacket)
 	} else if appPacket.IPProto == packet.IPProtocolUDP {
-		err = d.ProcessApplicationUDPPacket(appPacket)
+		length := uint32(len(p.Buffer))
+		buffer := p.Buffer
+		p.QueueHandle.SetVerdict2(uint32(p.QueueHandle.QueueNum), 1, uint32(p.Mark), length, uint32(p.ID), buffer)
+		return
+
+		//err = d.ProcessApplicationUDPPacket(appPacket)
 	} else {
 		err = fmt.Errorf("invalid ip protocol: %d", appPacket.IPProto)
 	}
