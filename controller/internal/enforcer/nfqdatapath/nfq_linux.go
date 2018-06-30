fdeff992ae17ae23a7125166619f5e607902bfc6
Varun Kulkarni <var.kulkarni@gmail.com>
2018-06-29 18:04:13 -0700

    revert back to usual behavior

--- controller/internal/enforcer/nfqdatapath/nfq_linux.go
+++ controller/internal/enforcer/nfqdatapath/nfq_linux.go
@@ -81,12 +81,7 @@ func (d *Datapath) processNetworkPacketsFromNFQ(p *nfqueue.NFPacket) {
 	} else if netPacket.IPProto == packet.IPProtocolTCP {
 		err = d.processNetworkTCPPackets(netPacket)
 	} else if netPacket.IPProto == packet.IPProtocolUDP {
-		length := uint32(len(p.Buffer))
-		buffer := p.Buffer
-		p.QueueHandle.SetVerdict2(uint32(p.QueueHandle.QueueNum), 0, uint32(p.Mark), length, uint32(p.ID), buffer)
-		zap.L().Debug("Varks: Set verdict to 0 (Net)", zap.String("flow", netPacket.L4FlowHash()))
-		return
-		//err = d.ProcessNetworkUDPPacket(netPacket)
+		err = d.ProcessNetworkUDPPacket(netPacket)
 	} else {
 		err = fmt.Errorf("invalid ip protocol: %d", netPacket.IPProto)
 	}
@@ -129,12 +124,7 @@ func (d *Datapath) processApplicationPacketsFromNFQ(p *nfqueue.NFPacket) {
 	} else if appPacket.IPProto == packet.IPProtocolTCP {
 		err = d.processApplicationTCPPackets(appPacket)
 	} else if appPacket.IPProto == packet.IPProtocolUDP {
-		length := uint32(len(p.Buffer))
-		buffer := p.Buffer
-		p.QueueHandle.SetVerdict2(uint32(p.QueueHandle.QueueNum), 0, uint32(p.Mark), length, uint32(p.ID), buffer)
-		zap.L().Debug("Varks: Set verdict to 0 (App)", zap.String("flow", appPacket.L4FlowHash()))
-		return
-		//err = d.ProcessApplicationUDPPacket(appPacket)
+		err = d.ProcessApplicationUDPPacket(appPacket)
 	} else {
 		err = fmt.Errorf("invalid ip protocol: %d", appPacket.IPProto)
 	}
