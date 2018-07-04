e740d781c235148683afd2fdbbbab91273fee567
Varun Kulkarni <var.kulkarni@gmail.com>
2018-07-03 18:27:49 -0700

    revert travis test.sh to normal

--- .test.sh
+++ .test.sh
@@ -6,9 +6,9 @@ echo "" > coverage.txt
 ./mockgen.sh
 
 for d in $(go list ./... | grep -v mock); do
-    sudo -E env "PATH=$PATH" go test -race -tags test -coverprofile=profile.out -covermode=atomic $d
+    go test -race -tags test -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
-        sudo rm profile.out
+        rm profile.out
     fi
 done
