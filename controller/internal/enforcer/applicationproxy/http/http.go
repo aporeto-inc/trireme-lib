29dfff1f42b36ba4856fb72b22f283c25e2cbfdb
Dimitri Stiliadis <dstiliadis@gmail.com>
2018-07-06 20:52:47 -0700

    Revert "Debug messages for CA pools"
    This reverts commit 09af37a25c497649265a70432750bb4f0ec82e0f.

--- controller/internal/enforcer/applicationproxy/http/http.go
+++ controller/internal/enforcer/applicationproxy/http/http.go
@@ -132,9 +132,6 @@ func (p *Config) RunNetworkServer(ctx context.Context, l net.Listener, encrypted
 				RootCAs:            p.ca,
 				InsecureSkipVerify: false,
 			})
-			for _, s := range p.ca.Subjects() {
-				fmt.Println("At connectiong added subject", string(s))
-			}
 			return tlsConn, nil
 		},
 	}
