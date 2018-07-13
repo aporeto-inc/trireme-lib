4027bc2dc56e442e184ec61d097ca534aa8385c6
Amit Limaye <amit@aporeto.com>
2018-07-13 15:20:07 -0700

    added test for sucessful case

--- monitor/extractors/uid_test.go
+++ monitor/extractors/uid_test.go
@@ -2,17 +2,37 @@ package extractors
 
 import (
 	"reflect"
+	"strconv"
 	"testing"
 
 	"go.aporeto.io/trireme-lib/common"
 	"go.aporeto.io/trireme-lib/policy"
 )
 
+func createDummyPolicy(event *common.EventInfo) *policy.PURuntime {
+	runtimeTags := policy.NewTagStore()
+	runtimeTags.AppendKeyValue("@sys:test", "valid")
+	runtimeIps := policy.ExtendedMap{"bridge": "0.0.0.0/0"}
+	options := &policy.OptionsType{
+		CgroupName: event.PUID,
+		CgroupMark: strconv.Itoa(101),
+		UserID:     event.PUID,
+		Services:   nil,
+	}
+	return policy.NewPURuntime(event.Name, int(event.PID), "", runtimeTags, runtimeIps, common.UIDLoginPU, options)
+}
 func TestUIDMetadataExtractor(t *testing.T) {
 	type args struct {
 		event *common.EventInfo
 	}
-
+	e := &common.EventInfo{
+		PID:      100,
+		Name:     "TestPU",
+		Tags:     []string{"test=valid"},
+		PUID:     "TestPU",
+		Services: nil,
+		PUType:   common.LinuxProcessPU,
+	}
 	tests := []struct {
 		name    string
 		args    args
@@ -29,6 +49,16 @@ func TestUIDMetadataExtractor(t *testing.T) {
 			want:    nil,
 			wantErr: true,
 		},
+		{
+			name: "Valid Tags",
+			args: args{
+				event: e,
+			},
+			want:    createDummyPolicy(e),
+			wantErr: false,
+		},
+
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -37,6 +67,7 @@ func TestUIDMetadataExtractor(t *testing.T) {
 				t.Errorf("UIDMetadataExtractor() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("UIDMetadataExtractor() = %v, want %v", got, tt.want)
 			}
