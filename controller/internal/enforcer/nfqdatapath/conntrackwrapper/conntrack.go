379136ff1c4fbb17b6cf82d08da6c80d4428b64b
Amit Limaye <amit@aporeto.com>
2018-08-07 17:05:56 -0700

    struct name

--- controller/internal/enforcer/nfqdatapath/conntrackwrapper/conntrack.go
+++ controller/internal/enforcer/nfqdatapath/conntrackwrapper/conntrack.go
@@ -9,12 +9,14 @@ type conntrackwrapper struct {
 	conntrackHdl conntrack.Conntrack
 }
 
+// New returns a handle to ConntrackWrapper
 func New() ConntrackWrapper {
-	return &conntrack{
+	return &conntrackwrapper{
 		conntrackHdl: conntrack.NewHandle(),
 	}
 }
 
+// ConntrackTableUpdateMark calls the netlink-go function. Indirection allows us to decouple the linux specific implementation
 func (c *conntrack) ConntrackTableUpdateMark(ipSrc, ipDst string, protonum uint8, srcport, dstport uint16, newmark uint32) error {
 	return c.conntrackHdl.ConntrackTableUpdateMark(ipSrc, ipDst, protonum, srcport, dstport, newmark)
 }
--- controller/internal/enforcer/nfqdatapath/conntrackwrapper/conntrack_nonlinux.go
+++ controller/internal/enforcer/nfqdatapath/conntrackwrapper/conntrack_nonlinux.go
@@ -6,7 +6,7 @@ type conntrackwrapper struct {
 }
 
 func New() ConntrackWrapper {
-	return &conntrack{}
+	return &conntrackwrapper{}
 }
 
 func (c *conntrack) ConntrackTableUpdateMark(ipSrc, ipDst string, protonum uint8, srcport, dstport uint16, newmark uint32) error {
