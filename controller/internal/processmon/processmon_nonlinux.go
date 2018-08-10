b52035c078024b69ae7f61e8130dd4bad88cd6d8
Amit Limaye <amit@aporeto.com>
2018-08-10 10:16:35 -0700

    compilation errors

--- controller/internal/processmon/processmon_nonlinux.go
+++ controller/internal/processmon/processmon_nonlinux.go
@@ -2,8 +2,9 @@
 
 package processmon
 
-type process struct {
+import "go.aporeto.io/trireme-lib/controller/internal/enforcer/utils/rpcwrapper"
 
+type process struct {
 }
 
 // KillProcess  unimplemented implement per platform
@@ -11,12 +12,12 @@ func (p *process) KillProcess(contextID string) {
 	return
 }
 
-// LaunchProcess unimplemented implement per platform to launch a new copy of the process  
-LaunchProcess(contextID string, refPid int, refNsPath string, rpchdl rpcwrapper.RPCClient, arg string, statssecret string, procMountPoint string) error {
-		return nil
+// LaunchProcess unimplemented implement per platform to launch a new copy of the process
+func (p *process) LaunchProcess(contextID string, refPid int, refNsPath string, rpchdl rpcwrapper.RPCClient, arg string, statssecret string, procMountPoint string) error {
+	return nil
 }
 
-// SetLogParameters unimplemented pass log parameters for the launched process 
-SetLogParameters(logToConsole, logWithID bool, logLevel string, logFormat string, compressedTags bool) {
+// SetLogParameters unimplemented pass log parameters for the launched process
+func (p *process) SetLogParameters(logToConsole, logWithID bool, logLevel string, logFormat string, compressedTags bool) {
 	return
 }
