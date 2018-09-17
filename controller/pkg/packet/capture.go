ab0fe8918e475751b492dfd53738468c624253d1
mehul chadha <mehul@aporeto.com>
2018-09-17 10:01:06 -0700

    fixing some bugs

--- controller/pkg/packet/capture.go
+++ controller/pkg/packet/capture.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
-	"sync"
 	"time"
 
 	"go.aporeto.io/trireme-lib/controller/constants"
@@ -13,34 +12,33 @@ import (
 )
 
 type Flows struct {
-	sync.RWMutex
 	Flows []Flow `json:"flows"`
 }
 
 type Flow struct {
-	SrcIP         string `json:"src"`
-	DstIP         string `json:"dst"`
-	SrcPort       uint16 `json:"sport"`
-	DstPort       uint16 `json:"dport"`
-	Proto         uint8  `json:"proto"`
-	Packets       uint16 `json:"packets"`
-	packetsDumped uint16
+	SrcIP   string `json:"src"`
+	DstIP   string `json:"dst"`
+	SrcPort uint16 `json:"sport"`
+	DstPort uint16 `json:"dport"`
+	Proto   uint8  `json:"proto"`
+	Packets uint32 `json:"packets"`
 }
 
 var flows Flows
-var lastUpdated time.Time
+var packetsDumped []uint32
 var writePCAP *os.File
+var channel chan Flows
 
 func init() {
 	var err error
-	lastUpdated = time.Now()
-	updateFlows()
+
 	pcapFile := "/var/lib/aporeto/capture" + os.Getenv(constants.EnvLogID)
 	writePCAP, err = os.OpenFile(pcapFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0660)
 	if err != nil {
 		return
 	}
 
+	channel = make(chan Flows)
 	go updateFlows()
 }
 
@@ -48,8 +46,14 @@ func updateFlows() {
 	var lastModified time.Time
 	var jsonFile *os.File
 	var err error
+	var flowLocal Flows
 
 	for ; ; time.Sleep(5 * time.Second) {
+		if jsonFile != nil {
+			jsonFile.Close()
+			jsonFile = nil
+		}
+
 		if jsonFile == nil {
 			jsonFile, err = os.Open("/var/lib/aporeto/inputflow.json")
 			if err != nil {
@@ -68,27 +72,26 @@ func updateFlows() {
 			continue
 		}
 
+		lastModified = modTime
 		byteVal, err := ioutil.ReadAll(jsonFile)
 		if err != nil {
 			zap.L().Error("Read json returned error", zap.Error(err))
 			continue
 		}
 
-		flows.Lock()
-		err = json.Unmarshal(byteVal, &flows)
-		flows.Unlock()
-
+		err = json.Unmarshal(byteVal, &flowLocal)
 		if err != nil {
 			zap.L().Error("error json unmarshal", zap.Error(err))
 			continue
 		}
 
-		lastModified = modTime
+		channel <- flowLocal
 	}
 }
 
 func writeCapture(buffer []byte, index int) {
-	if writePCAP != nil && flows.Flows[index].packetsDumped < flows.Flows[index].Packets {
+
+	if writePCAP != nil && packetsDumped[index] < flows.Flows[index].Packets {
 
 		var header []byte = []byte{0xab, 0xcd, 0xef, 0x00, 0x01, 0x02, 0xbc, 0xcd, 0xef, 0x00, 0x01, 0x02, 0x08, 0x00}
 
@@ -96,14 +99,20 @@ func writeCapture(buffer []byte, index int) {
 			zap.L().Error("Could not write pcap file")
 		}
 
-		flows.Flows[index].packetsDumped += 1
+		packetsDumped[index] += 1
 	}
 }
 
 func (p *Packet) DebugCapture(buffer []byte) {
 
-	flows.RLock()
-	defer flows.RUnlock()
+	// Update flows if there is a message on a channel
+	select {
+
+	case flows = <-channel:
+		packetsDumped = make([]uint32, len(flows.Flows))
+	default:
+
+	}
 
 	for i := 0; i < len(flows.Flows); i++ {
 		pktSrcIP := p.SourceAddress.String()
