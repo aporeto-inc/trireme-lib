6d246be3277614df83f6720c785447abec4e1d35
Satyam Sinha <s@aporeto.com>
2018-09-24 22:54:53 -0700

    Fixed: typo

--- controller/internal/enforcer/nfqdatapath/utils.go
+++ controller/internal/enforcer/nfqdatapath/utils.go
@@ -91,7 +91,7 @@ func (d *Datapath) reportExternalServiceFlowCommon(context *pucontext.PUContext,
 	}
 
 	dropReason := ""
-	if report.Action.Rejected() || packet.Action.Rejected() {
+	if report.Action.Rejected() || actual.Action.Rejected() {
 		dropReason = collector.PolicyDrop
 	}
 
