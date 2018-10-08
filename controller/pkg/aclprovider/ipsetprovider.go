454f1b869b8dd98aa419b1a9286fbc329a22c805
Amit Limaye <amit@aporeto.com>
2018-10-08 05:50:23 -0700

    Changes with master

--- controller/pkg/aclprovider/ipsetprovider.go
+++ controller/pkg/aclprovider/ipsetprovider.go
@@ -6,6 +6,23 @@ import "github.com/bvandewalle/go-ipset/ipset"
 
 type goIpsetProvider struct{}
 
+// IpsetProvider returns a fabric for Ipset.
+type IpsetProvider interface {
+	NewIpset(name string, hasht string, p *ipset.Params) (Ipset, error)
+	DestroyAll() error
+}
+
+// Ipset is an abstraction of all the methods an implementation of userspace
+// ipsets need to provide.
+type Ipset interface {
+	Add(entry string, timeout int) error
+	AddOption(entry string, option string, timeout int) error
+	Del(entry string) error
+	Destroy() error
+	Flush() error
+	Test(entry string) (bool, error)
+}
+
 // NewIpset returns an IpsetProvider interface based on the go-ipset
 // external package.
 func (i *goIpsetProvider) NewIpset(name string, hasht string, p *ipset.Params) (Ipset, error) {
--- controller/pkg/aclprovider/ipsetprovider_nonlinux.go
+++ controller/pkg/aclprovider/ipsetprovider_nonlinux.go
@@ -1,4 +1,5 @@
 // +build windows
+// +build windows
 
 package provider
 
@@ -10,7 +11,22 @@ import (
 
 type goIpsetProvider struct{}
 
-type Ipset struct{}
+// IpsetProvider returns a fabric for Ipset.
+type IpsetProvider interface {
+	NewIpset(name string, hasht string, p *ipset.Params) (Ipset, error)
+	DestroyAll() error
+}
+
+// Ipset is an abstraction of all the methods an implementation of userspace
+// ipsets need to provide.
+type Ipset interface {
+	Add(entry string, timeout int) error
+	AddOption(entry string, option string, timeout int) error
+	Del(entry string) error
+	Destroy() error
+	Flush() error
+	Test(entry string) (bool, error)
+}
 
 // NewIpset returns an IpsetProvider interface based on the go-ipset
 // external package.
