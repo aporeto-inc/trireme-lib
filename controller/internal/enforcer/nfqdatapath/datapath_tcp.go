2fd23f918f28f71d9378a76944907ec8845b8f33
Amit Limaye <amit@aporeto.com>
2018-10-16 11:45:27 -0700

    Comment for loopback case

--- controller/internal/enforcer/nfqdatapath/datapath_tcp.go
+++ controller/internal/enforcer/nfqdatapath/datapath_tcp.go
@@ -401,6 +401,8 @@ func (d *Datapath) processApplicationAckPacket(tcpPacket *packet.Packet, context
 		// packets after the first data packet, that might be already in the queue
 		// will be transmitted through the kernel directly. Service connections are
 		// delegated to the service module
+		// Check for both ends not being loopback. in such a case we will let the network ack
+		// update the connmark on the flow
 		if !conn.ServiceConnection &&
 			tcpPacket.SourceAddress.String() != tcpPacket.DestinationAddress.String() &&
 			!(tcpPacket.SourceAddress.IsLoopback() && tcpPacket.DestinationAddress.IsLoopback()) {
