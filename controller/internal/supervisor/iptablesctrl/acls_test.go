f749344c7b6900da327035d596549a59b71e0f7f
Varun Kulkarni <var.kulkarni@gmail.com>
2018-10-19 12:59:55 -0700

    fix merge conflict

--- controller/internal/supervisor/iptablesctrl/acls_test.go
+++ controller/internal/supervisor/iptablesctrl/acls_test.go
@@ -7,7 +7,7 @@ import (
 
 	"go.aporeto.io/trireme-lib/monitor/extractors"
 
-	"github.com/bvandewalle/go-ipset/ipset"
+	"github.com/aporeto-inc/go-ipset/ipset"
 	. "github.com/smartystreets/goconvey/convey"
 	"go.aporeto.io/trireme-lib/controller/constants"
 	"go.aporeto.io/trireme-lib/controller/internal/portset"
