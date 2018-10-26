c82d6aa644934ebe139479a4288728b0a1ca9dfd
Amit Limaye <amit@aporeto.com>
2018-10-25 18:40:04 -0700

    adding groupname as tags

--- plugins/pam/uidmonitorpam.go
+++ plugins/pam/uidmonitorpam.go
@@ -14,9 +14,12 @@ int is_root(char *user);
 */
 import "C"
 import (
+	"bufio"
 	"fmt"
 	"log/syslog"
 	"os"
+	"os/user"
+	"strings"
 
 	"go.aporeto.io/trireme-lib/common"
 	"go.aporeto.io/trireme-lib/monitor/remoteapi/client"
@@ -26,12 +29,37 @@ import (
 //export pam_sm_open_session
 func pam_sm_open_session(pamh *C.pam_handle_t, flags, argc int, argv **C.char) C.int {
 	C.initLog()
-	user := C.get_user(pamh)
+	username := C.get_user(pamh)
 	service := C.get_service(pamh)
 	metadatamap := []string{}
-	userstring := "user=" + C.GoString(user)
+	goUserName := C.GoString(username)
+	userstring := "user=" + goUserName
 	metadatamap = append(metadatamap, userstring)
+	slog, _ := syslog.New(syslog.LOG_ALERT|syslog.LOG_AUTH, "mypam")
+	defer func() {
+		_ = slog.Close()
+	}()
+	userhandle, err := user.Lookup(goUserName)
+	if err != nil {
+		slog.Alert("Invalid username" + goUserName)
+	}
+	if groups, err := userhandle.GroupIds(); err != nil {
+		slog.Alert("Unable to get group list" + err.Error())
+	} else {
+		for _, group := range groups {
+			//metadatamap = append(metadatamap, "groupid_"+strconv.Itoa(i)+"="+group)
+			// if grpEntry, err := user.LookupGroup(group); err == nil {
+			// 	metadatamap = append(metadatamap, "groupname"+strconv.Itoa(i)+"="+grpEntry.Name)
+			// } else {
+			// 	slog.Alert("HERE6" + err.Error())
+			// }
+			//hack lookupgroup is returning errors
+			if groupName, err := findgroupname(group); err == nil {
+				metadatamap = append(metadatamap, "groupname="+groupName)
+			}
 
+		}
+	}
 	if service != nil {
 		metadatamap = append(metadatamap, "SessionType="+C.GoString(service))
 	} else {
@@ -40,21 +68,17 @@ func pam_sm_open_session(pamh *C.pam_handle_t, flags, argc int, argv **C.char) C
 
 	request := &common.EventInfo{
 		PUType:    common.UIDLoginPU,
-		PUID:      C.GoString(user),
-		Name:      "login-" + C.GoString(user),
+		PUID:      goUserName,
+		Name:      "login-" + goUserName,
 		PID:       int32(os.Getpid()),
 		Tags:      metadatamap,
 		EventType: "start",
 	}
 
-	if C.is_root(user) == 1 {
+	if C.is_root(username) == 1 {
 		//Do nothing this is login shell account
 	} else {
 		//Do something
-		slog, _ := syslog.New(syslog.LOG_ALERT|syslog.LOG_AUTH, "mypam")
-		defer func() {
-			_ = slog.Close()
-		}()
 
 		client, err := client.NewClient(common.TriremeSocket)
 		if err != nil {
@@ -78,6 +102,34 @@ func pam_sm_close_session(pamh *C.pam_handle_t, flags, argc int, argv **C.char) 
 	slog.Alert("pam_sm_close_session") // nolint
 	slog.Close()                       // nolint
 	return C.PAM_SUCCESS
+}
+
+func findgroupname(gid string) (string, error) {
+	f, err := os.Open("/etc/group")
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	bs := bufio.NewScanner(f)
+	for bs.Scan() {
+		line := bs.Text()
+		line = strings.TrimSpace(line)
+		if len(line) == 0 || line[0] == '#' {
+
+			continue
+
+		}
+		entry := strings.Split(string(line), ":")
+		if len(entry) < 3 {
+			continue
+		}
+		if strings.Compare(entry[2], gid) == 0 {
+
+			return entry[0], nil
+		}
+
+	}
+	return "", fmt.Errorf("group not found %s", gid)
 }
 
 func main() {
