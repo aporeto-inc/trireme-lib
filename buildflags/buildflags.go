898014eb53020cab141876415ce8818ad1ada94e
Satyam Sinha <s@aporeto.com>
2018-11-01 17:00:47 -0700

    New: Allow distro to be checked

--- buildflags/buildflags.go
+++ buildflags/buildflags.go
@@ -4,8 +4,17 @@ package buildflags
 const (
 	Rhel6 = "rhel6"
 	Rhel5 = "rhel5"
-	None  = ""
 )
 
 // Distro is a variable set during the build which allows different behaviors
 var Distro string
+
+// IsRHEL6 returns true if the build flag was set for rhel6
+func IsRHEL6() bool {
+	return Distro == Rhel6
+}
+
+// IsRHEL5 returns true if the build flag was set for rhel5
+func IsRHEL5() bool {
+	return Distro == Rhel5
+}
