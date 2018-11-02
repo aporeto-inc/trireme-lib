fa85695f32d08fb9f9b13abf90831a157ead5c8d
Varun Kulkarni <var.kulkarni@gmail.com>
2018-11-02 16:18:39 -0700

    remove unused variable

--- buildflags/buildflags_rhel.go
+++ buildflags/buildflags_rhel.go
@@ -8,9 +8,6 @@ const (
 	Rhel5 = "rhel5"
 )
 
-// Distro is a variable set during the build which allows different behaviors
-var Distro string
-
 // IsRHEL6 returns true if the build flag was set for rhel6
 func IsRHEL6() bool {
 	return true
