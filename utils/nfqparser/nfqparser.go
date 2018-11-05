5c647729885fca8249476469d76dc417a4241e34
Sibi Chakravarthy <sibi@aporeto.com>
2018-11-05 15:25:12 -0800

    New: NFQParser

---
+++ utils/nfqparser/nfqparser.go
@@ -0,0 +1,141 @@
+package nfqparser
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strings"
+	"sync"
+)
+
+// NFQParser holds nfqparser fields
+type NFQParser struct {
+	nfqStr string
+	// NOTE: For unit test
+	test     bool
+	contents map[string]NFQLayout
+
+	sync.Mutex
+}
+
+// NewNFQParser returns nfqparser handler
+func NewNFQParser() *NFQParser {
+
+	return &NFQParser{
+		contents: make(map[string]NFQLayout),
+	}
+}
+
+// Synchronize reads from file and parses it
+func (n *NFQParser) Synchronize() error {
+
+	n.Lock()
+	defer n.Unlock()
+
+	data := []byte(testNFQData)
+	var err error
+
+	if !n.test {
+		data, err = ioutil.ReadFile(nfqFilePath)
+		if err != nil {
+			return err
+		}
+	}
+
+	n.nfqStr = string(data)
+
+	scanner := bufio.NewScanner(bytes.NewReader(data))
+	for scanner.Scan() {
+		line := scanner.Text()
+		trimmedLine := strings.TrimSpace(line)
+		lineParts := strings.Fields(trimmedLine)
+		newNFQ := makeNFQLayout(lineParts)
+
+		n.contents[newNFQ.QueueNum] = newNFQ
+	}
+
+	return nil
+}
+
+// RetrieveByQueue returns layout for a specific queue number
+func (n *NFQParser) RetrieveByQueue(queueNum string) *NFQLayout {
+
+	n.Lock()
+	defer n.Unlock()
+
+	content, ok := n.contents[queueNum]
+	if ok {
+		return &content
+	}
+
+	return nil
+}
+
+// RetrieveByField returns layout in string representation
+func (n *NFQParser) RetrieveByField(field Field) string {
+
+	n.Lock()
+	defer n.Unlock()
+
+	var fieldStr string
+	for _, key := range n.sortedKeys() {
+		content := n.contents[key]
+		switch field {
+		case FieldPeerPortID:
+			fieldStr = fieldStr + content.PeerPortID
+		case FieldQueueTotal:
+			fieldStr = fieldStr + content.QueueTotal
+		case FieldCopyMode:
+			fieldStr = fieldStr + content.CopyMode
+		case FieldCopyRange:
+			fieldStr = fieldStr + content.CopyRange
+		case FieldQueueDropped:
+			fieldStr = fieldStr + content.QueueDropped
+		case FieldUserDropped:
+			fieldStr = fieldStr + content.UserDropped
+		case FieldIDSequene:
+			fieldStr = fieldStr + content.IDSequene
+		default:
+			return fmt.Sprintf("Unknown field")
+		}
+	}
+
+	return fieldStr
+}
+
+// String returns string renresentation of nfqueue data
+func (n *NFQParser) String() string {
+
+	n.Lock()
+	defer n.Unlock()
+
+	return n.nfqStr
+}
+
+func (n *NFQParser) sortedKeys() []string {
+
+	var keys []string
+	for key := range n.contents {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+
+	return keys
+}
+
+func makeNFQLayout(data []string) NFQLayout {
+
+	newNFQ := NFQLayout{}
+	newNFQ.QueueNum = data[0]
+	newNFQ.PeerPortID = data[1]
+	newNFQ.QueueTotal = data[2]
+	newNFQ.CopyMode = data[3]
+	newNFQ.CopyRange = data[4]
+	newNFQ.QueueDropped = data[5]
+	newNFQ.UserDropped = data[6]
+	newNFQ.IDSequene = data[7]
+
+	return newNFQ
+}
---
+++ utils/nfqparser/nfqparser_test.go
@@ -0,0 +1,150 @@
+package nfqparser
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func testProperLayout() *NFQLayout {
+
+	return &NFQLayout{
+		QueueNum:     "6",
+		PeerPortID:   "4025478776",
+		QueueTotal:   "0",
+		CopyMode:     "2",
+		CopyRange:    "65531",
+		QueueDropped: "0",
+		UserDropped:  "0",
+		IDSequene:    "3",
+	}
+}
+
+func TestNFQParserRetrieveByQueue(t *testing.T) {
+
+	Convey("Given I create a new nfqparser instance", t, func() {
+		nfqParser := NewNFQParser()
+		nfqParser.test = true
+
+		Convey("Given I try to synchronize data", func() {
+			err := nfqParser.Synchronize()
+
+			Convey("Then I should not get any errors", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("Given I try to retrieve data for a queue", func() {
+				queueData := nfqParser.RetrieveByQueue("6")
+
+				Convey("Then queue data should match", func() {
+					So(queueData, ShouldResemble, testProperLayout())
+				})
+			})
+
+			Convey("Given I try to retrieve data for a queue and compare a specific field", func() {
+				queueData := nfqParser.RetrieveByQueue("6")
+
+				Convey("Then queue data should match", func() {
+					So(queueData.CopyRange, ShouldEqual, testProperLayout().CopyRange)
+				})
+			})
+
+			Convey("Given I try to retrieve data for a queue with different expected data", func() {
+				queueData := nfqParser.RetrieveByQueue("1")
+
+				Convey("Then queue data should match", func() {
+					So(queueData, ShouldNotResemble, testProperLayout())
+				})
+			})
+
+			Convey("Given I try to retrieve data for a invalid queue num", func() {
+				queueData := nfqParser.RetrieveByQueue("9")
+
+				Convey("Then queue data should match", func() {
+					So(queueData, ShouldBeNil)
+				})
+			})
+		})
+	})
+}
+
+func TestNFQParserRetrieveByField(t *testing.T) {
+
+	Convey("Given I create a new nfqparser instance", t, func() {
+		nfqParser := NewNFQParser()
+		nfqParser.test = true
+
+		Convey("Given I try to synchronize data", func() {
+			err := nfqParser.Synchronize()
+
+			Convey("Then I should not get any errors", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("Given I try to retrieve data for a portid", func() {
+				portID := nfqParser.RetrieveByField(FieldPeerPortID)
+
+				Convey("Then portID should match", func() {
+					So(portID, ShouldEqual, "132063333107750388139856926337506853605545056347323018840254787762806986372")
+				})
+			})
+
+			Convey("Given I try to retrieve data for a id sequence", func() {
+				portID := nfqParser.RetrieveByField(FieldIDSequene)
+
+				Convey("Then id sequence should match", func() {
+					So(portID, ShouldEqual, "00100231")
+				})
+			})
+
+			Convey("Given I try to retrieve unknown field", func() {
+				unknown := nfqParser.RetrieveByField(Field(35))
+
+				Convey("Then queue data should match", func() {
+					So(unknown, ShouldEqual, "Unknown field")
+				})
+			})
+		})
+	})
+}
+
+func TestNFQParserString(t *testing.T) {
+
+	Convey("Given I create a new nfqparser instance", t, func() {
+		nfqParser := NewNFQParser()
+		nfqParser.test = true
+
+		Convey("Given I try to synchronize data", func() {
+			err := nfqParser.Synchronize()
+
+			Convey("Then I should not get any errors", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("Given I try to get string representation", func() {
+				strData := nfqParser.String()
+
+				Convey("Then queue data should match", func() {
+					So(strData, ShouldEqual, testNFQData)
+				})
+			})
+
+			Convey("Given I try to retrieve data for a queue with different expected data", func() {
+				queueData := nfqParser.RetrieveByQueue("6")
+
+				Convey("Then queue data should match", func() {
+					So(queueData, ShouldResemble, testProperLayout())
+				})
+
+				Convey("Given I try to get string representaion of particular queue number", func() {
+					queueData := nfqParser.RetrieveByQueue("6")
+
+					Convey("Then queue data should match", func() {
+						So(queueData.String(), ShouldEqual, fmt.Sprintf("%s", testProperLayout()))
+					})
+				})
+			})
+		})
+	})
+}
