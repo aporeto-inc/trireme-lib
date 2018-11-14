92895c9f083ec7ad1df075ff62cca04c932e2774
Varun Kulkarni <var.kulkarni@gmail.com>
2018-11-13 16:25:15 -0800

    add unit tests

--- monitor/internal/kubernetes/handler_test.go
+++ monitor/internal/kubernetes/handler_test.go
@@ -5,11 +5,15 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	"go.aporeto.io/trireme-lib/utils/cgnetcls/mockcgnetcls"
+
 	"go.aporeto.io/trireme-lib/common"
 	"go.aporeto.io/trireme-lib/monitor/config"
 	"go.aporeto.io/trireme-lib/monitor/extractors"
 	dockermonitor "go.aporeto.io/trireme-lib/monitor/internal/docker"
 	"go.aporeto.io/trireme-lib/policy"
+	"go.aporeto.io/trireme-lib/utils/cgnetcls"
 	api "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 	kubefake "k8s.io/client-go/kubernetes/fake"
@@ -340,3 +344,226 @@ func TestKubernetesMonitor_RefreshPUs(t *testing.T) {
 		})
 	}
 }
+
+func Test_isPodInfraContainer(t *testing.T) {
+	type args struct {
+		runtime policy.RuntimeReader
+	}
+
+	puRuntimeWithTags := func(tags map[string]string) *policy.PURuntime {
+		puRuntime := policy.NewPURuntimeWithDefaults()
+		puRuntime.SetTags(policy.NewTagStoreFromMap(tags))
+		return puRuntime
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Test if runtime has kubernetes infra pod tags",
+			args: args{
+				runtime: puRuntimeWithTags(map[string]string{
+					"@usr:io.kubernetes.container.name": "POD",
+				},
+				),
+			},
+			want: true,
+		},
+		{
+			name: "Test if runtime does not have kubernetes infra pod tags",
+			args: args{
+				runtime: puRuntimeWithTags(map[string]string{
+					"key": "value",
+				},
+				),
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPodInfraContainer(tt.args.runtime); got != tt.want {
+				t.Errorf("isPodInfraContainer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestKubernetesMonitor_setupHostMode(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	netcls := mockcgnetcls.NewMockCgroupnetcls(ctrl)
+
+	puRuntime := func() *policy.PURuntime {
+
+		pur := policy.NewPURuntime("", 1, "", nil, nil, common.LinuxProcessPU, nil)
+		pur.SetOptions(policy.OptionsType{
+			CgroupMark: "100",
+		})
+		return pur
+	}
+
+	puRuntimeBadCgroup := func() *policy.PURuntime {
+
+		pur2 := policy.NewPURuntime("", 1, "", nil, nil, common.LinuxProcessPU, nil)
+		return pur2
+	}
+
+	type fields struct {
+		dockerMonitor       *dockermonitor.DockerMonitor
+		kubeClient          kubernetes.Interface
+		localNode           string
+		handlers            *config.ProcessorConfig
+		cache               *cache
+		kubernetesExtractor extractors.KubernetesMetadataExtractorType
+		podStore            kubecache.Store
+		podController       kubecache.Controller
+		podControllerStop   chan struct{}
+		netcls              cgnetcls.Cgroupnetcls
+		enableHostPods      bool
+	}
+	type args struct {
+		puID        string
+		runtimeInfo policy.RuntimeReader
+		event       common.Event
+		pause       bool
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+
+		// TODO: Add test cases.
+		{
+			name: "Test programming netcls for pause containers",
+			fields: fields{
+				netcls: netcls,
+			},
+			args: args{
+				puID:        "abcd",
+				runtimeInfo: puRuntime(),
+				event:       common.EventStart,
+				pause:       true,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test programming netcls for other containers, pause false",
+			fields: fields{
+				netcls: netcls,
+			},
+			args: args{
+				puID:        "abcd",
+				runtimeInfo: puRuntime(),
+				event:       common.EventStart,
+				pause:       true,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test programming netcls with invalid cgroup mark",
+			fields: fields{
+				netcls: netcls,
+			},
+			args: args{
+				puID:        "abcd",
+				runtimeInfo: puRuntimeBadCgroup(),
+				event:       common.EventStart,
+				pause:       true,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Test programming netcls with invalid cgroup mark",
+			fields: fields{
+				netcls: netcls,
+			},
+			args: args{
+				puID:        "abcd",
+				runtimeInfo: puRuntime(),
+				event:       common.EventStop,
+				pause:       true,
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			netcls.EXPECT().Creategroup(tt.args.puID).Return(nil).MinTimes(0)
+			netcls.EXPECT().AssignMark(tt.args.puID, gomock.Any()).Return(nil).MinTimes(0)
+			netcls.EXPECT().DeleteCgroup(tt.args.puID).Return(nil).MinTimes(0)
+			netcls.EXPECT().AddProcess(tt.args.puID, tt.args.runtimeInfo.Pid()).Return(nil).MinTimes(0)
+
+			m := &KubernetesMonitor{
+				dockerMonitor:       tt.fields.dockerMonitor,
+				kubeClient:          tt.fields.kubeClient,
+				localNode:           tt.fields.localNode,
+				handlers:            tt.fields.handlers,
+				cache:               tt.fields.cache,
+				kubernetesExtractor: tt.fields.kubernetesExtractor,
+				podStore:            tt.fields.podStore,
+				podController:       tt.fields.podController,
+				podControllerStop:   tt.fields.podControllerStop,
+				netcls:              tt.fields.netcls,
+				enableHostPods:      tt.fields.enableHostPods,
+			}
+			if err := m.setupHostMode(tt.args.puID, tt.args.runtimeInfo, tt.args.event, tt.args.pause); (err != nil) != tt.wantErr {
+				t.Errorf("KubernetesMonitor.setupHostMode() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+// func TestKubernetesMonitor_findPauseContainer(t *testing.T) {
+// 	type fields struct {
+// 		dockerMonitor       *dockermonitor.DockerMonitor
+// 		kubeClient          kubernetes.Interface
+// 		localNode           string
+// 		handlers            *config.ProcessorConfig
+// 		cache               *cache
+// 		kubernetesExtractor extractors.KubernetesMetadataExtractorType
+// 		podStore            kubecache.Store
+// 		podController       kubecache.Controller
+// 		podControllerStop   chan struct{}
+// 		netcls              cgnetcls.Cgroupnetcls
+// 		enableHostPods      bool
+// 	}
+// 	type args struct {
+// 		podName      string
+// 		podNamespace string
+// 		puID         chan string
+// 	}
+// 	tests := []struct {
+// 		name   string
+// 		fields fields
+// 		args   args
+// 	}{
+//
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			m := &KubernetesMonitor{
+// 				dockerMonitor:       tt.fields.dockerMonitor,
+// 				kubeClient:          tt.fields.kubeClient,
+// 				localNode:           tt.fields.localNode,
+// 				handlers:            tt.fields.handlers,
+// 				cache:               tt.fields.cache,
+// 				kubernetesExtractor: tt.fields.kubernetesExtractor,
+// 				podStore:            tt.fields.podStore,
+// 				podController:       tt.fields.podController,
+// 				podControllerStop:   tt.fields.podControllerStop,
+// 				netcls:              tt.fields.netcls,
+// 				enableHostPods:      tt.fields.enableHostPods,
+// 			}
+// 			m.findPauseContainer(tt.args.podName, tt.args.podNamespace, tt.args.puID)
+// 		})
+// 	}
+// }
