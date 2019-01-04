c5e4f54e66655df435e7d868cc9e4f3ddd9cd24d
Sibi Chakravarthy <sibi@aporeto.com>
2019-01-04 14:24:28 -0800

    Fix: Proper structure

--- controller/pkg/claimsheader/claimsheader.go
+++ controller/pkg/claimsheader/claimsheader.go
@@ -16,14 +16,14 @@ func NewClaimsHeader(opts ...Option) *ClaimsHeader {
 
 // ToBytes generates the 32-bit header in bytes
 //    0             1              2               3               4
-//   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
 //  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-// 	|CT|E|  H    |                 R (reserved)                     |
-// 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//  CT [0,1] - Compressed tag type
-//  E [2]    - Enryption enabled
-//  H [3:6]  - Handshake version
-//  R [4:31] - Unused currently
+//  |CT|E|  H    |                 R (reserved)                     |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  CT [0,1]  - Compressed tag type
+//  E  [2]    - Enryption enabled
+//  H  [3:6]  - Handshake version
+//  R  [4:31] - Unused currently
 func (c *ClaimsHeader) ToBytes() HeaderBytes {
 
 	claimsHeaderData := make([]byte, MaxHeaderLen)
