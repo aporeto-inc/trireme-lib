a7d1f39e903e3e370034caee1f383d5211b3f30c
Varun Kulkarni <var.kulkarni@gmail.com>
2019-01-31 21:46:58 -0800

    fix acls

--- controller/internal/supervisor/iptablesctrl/acls.go
+++ controller/internal/supervisor/iptablesctrl/acls.go
@@ -570,7 +570,7 @@ func (i *Instance) trapRules(appChain string, netChain string, isHostPU bool) []
 	rules = append(rules, []string{
 		i.appPacketIPTableContext, appChain,
 		"-m", "set", "--match-set", targetNetworkSet, "dst",
-		"-p", udpProto,
+		"-p", udpProto, "-m", "statistic", "--mode", "nth",
 		"--every", numPackets, "--packet", initialCount,
 		"-j", "NFQUEUE", "--queue-balance", i.fqc.GetApplicationQueueAckStr(),
 	})
