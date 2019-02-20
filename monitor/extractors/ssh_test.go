28c02707b828ca193e1380dec94833cfcc9aa731
Sibi Chakravarthy <sibi@aporeto.com>
2019-02-19 23:37:06 -0800

    Fix: Metadataextractor unitest

---
+++ monitor/extractors/ssh_test.go
@@ -0,0 +1,49 @@
+package extractors
+
+import (
+	"strconv"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"go.aporeto.io/trireme-lib/common"
+	"go.aporeto.io/trireme-lib/policy"
+	"go.aporeto.io/trireme-lib/utils/cgnetcls"
+)
+
+func testRuntime() *policy.PURuntime {
+
+	tags := policy.NewTagStore()
+	tags.AppendKeyValue("@sys:app", "web")
+	tags.AppendKeyValue("@app:ssh:app", "web")
+	tags.AppendKeyValue("$cert", "ss")
+
+	runtimeIps := policy.ExtendedMap{"bridge": "0.0.0.0/0"}
+	options := &policy.OptionsType{
+		CgroupName: "/1234",
+		CgroupMark: strconv.FormatUint(cgnetcls.MarkVal(), 10),
+		UserID:     "/1234",
+	}
+
+	return policy.NewPURuntime("curl", 1234, "", tags, runtimeIps, common.SSHSessionPU, options)
+}
+
+func TestSSHMetadataExtractor(t *testing.T) {
+
+	Convey("When I call the ssh metadata extrator", t, func() {
+
+		Convey("If all data are present", func() {
+			event := &common.EventInfo{
+				Name: "curl",
+				PID:  1234,
+				PUID: "/1234",
+				Tags: []string{"app=web", "$cert=ss"},
+			}
+
+			pu, err := SSHMetadataExtractor(event)
+			Convey("I should get no error and a valid PU runtime", func() {
+				So(err, ShouldBeNil)
+				So(pu, ShouldResemble, testRuntime())
+			})
+		})
+	})
+}
