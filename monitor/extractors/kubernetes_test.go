58854bd35ed6e4497bcf5176a296c17208f483ec
Marcus Heese <marcus@aporeto.com>
2019-02-25 12:09:30 -0800

    bringing back the original kubernetes metadata extractor

--- monitor/extractors/kubernetes_test.go
+++ monitor/extractors/kubernetes_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestDefaultKubernetesMetadataExtractor(t *testing.T) {
-	t.SkipNow()
+
 	pod1 := &api.Pod{}
 	pod1.SetName("test")
 	pod1.SetNamespace("ns")
@@ -17,11 +17,17 @@ func TestDefaultKubernetesMetadataExtractor(t *testing.T) {
 		"label": "one",
 	})
 
+	runtimeDocker := policy.NewPURuntimeWithDefaults()
+	tags := runtimeDocker.Tags()
+	tags.AppendKeyValue(KubernetesContainerNameIdentifier, "POD")
+	runtimeDocker.SetTags(tags)
+
 	runtimeResult := policy.NewPURuntimeWithDefaults()
-	runtimeResult.SetIPAddresses(policy.ExtendedMap{"bridge": ""})
-	tags := runtimeResult.Tags()
+	tags = runtimeResult.Tags()
 	tags.AppendKeyValue("label", "one")
+	tags.AppendKeyValue(UpstreamOldNameIdentifier, "test")
 	tags.AppendKeyValue(UpstreamNameIdentifier, "test")
+	tags.AppendKeyValue(UpstreamOldNamespaceIdentifier, "ns")
 	tags.AppendKeyValue(UpstreamNamespaceIdentifier, "ns")
 
 	runtimeResult.SetTags(tags)
@@ -40,35 +46,52 @@ func TestDefaultKubernetesMetadataExtractor(t *testing.T) {
 		{
 			name: "empty1",
 			args: args{
-				pod: &api.Pod{},
+				runtime: nil,
+				pod:     &api.Pod{},
 			},
 			wantErr: true,
 		},
 		{
 			name: "empty2",
 			args: args{
-				pod: nil,
+				runtime: policy.NewPURuntimeWithDefaults(),
+				pod:     nil,
 			},
 			wantErr: true,
 		},
 		{
+			name: "Simple test, non Kubernetes Container",
+			args: args{
+				runtime: policy.NewPURuntimeWithDefaults(),
+				pod:     pod1,
+			},
+			want:    nil,
+			want1:   false,
+			wantErr: false,
+		},
+		{
 			name: "Simple test",
 			args: args{
-				pod: pod1,
+				runtime: runtimeDocker,
+				pod:     pod1,
 			},
 			want:    runtimeResult,
+			want1:   true,
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := DefaultKubernetesMetadataExtractor(tt.args.pod)
+			got, got1, err := DefaultKubernetesMetadataExtractor(tt.args.runtime, tt.args.pod)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("DefaultKubernetesMetadataExtractor() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("DefaultKubernetesMetadataExtractor() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("DefaultKubernetesMetadataExtractor() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
