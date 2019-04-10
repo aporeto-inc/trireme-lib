496043adfa1fb3b2b17693ea674fcf641378d90d
Amit Limaye <amit@aporeto.com>
2019-04-09 22:03:32 -0700

    add sudo to go test so i can stat proc

--- .test.sh
+++ .test.sh
@@ -6,7 +6,7 @@ echo "" > coverage.txt
 ./mockgen.sh
 
 for d in $(go list ./... | grep -v mock); do
-    go test -race -tags test -coverprofile=profile.out -covermode=atomic $d
+    sudo go test -race -tags test -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
