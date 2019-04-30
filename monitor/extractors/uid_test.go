3a8d334ebdabb66045e4308a726b59d86c60c272
Marcus Heese <marcus@aporeto.com>
2019-04-30 13:46:10 -0700

    Fix empty tags from Kubernetes Labels and Annotations (#781)
    * ensure default kubernetes extractor does not add tags with empty values from k8s labels
    
    * fixing potentially empty tags from docker extractor
    
    * improving test coverage for docker metadata extractor
    
    * trigger new travis build
    
    * fixing ssh extractor test
    
    * fixing uid extractor test
    
    * adding more code coverage to unit test
    
    * increasing cgroup mark
    
    * no idea why I have to not increase this one
    
    * also decreassing cgroup mark again for this one

--- monitor/extractors/uid_test.go
+++ monitor/extractors/uid_test.go
@@ -16,7 +16,7 @@ func createDummyPolicy(event *common.EventInfo) *policy.PURuntime {
 	runtimeIps := policy.ExtendedMap{"bridge": "0.0.0.0/0"}
 	options := &policy.OptionsType{
 		CgroupName: event.PUID,
-		CgroupMark: strconv.Itoa(104),
+		CgroupMark: strconv.Itoa(105),
 		UserID:     event.PUID,
 		Services:   nil,
 	}
