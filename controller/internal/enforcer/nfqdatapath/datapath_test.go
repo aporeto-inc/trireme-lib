1768626065ddcdc33894570133940fc7c665921e
Amit Limaye <amit@aporeto.com>
2019-06-10 12:31:58 -0700

    Reversing param order

--- controller/internal/enforcer/nfqdatapath/datapath_test.go
+++ controller/internal/enforcer/nfqdatapath/datapath_test.go
@@ -4945,7 +4945,7 @@ func TestCheckConnectionDeletion(t *testing.T) {
 		So(err, ShouldBeNil)
 		tcpSynAckPacket, err := packet.New(0, synAckPacket, "0", true)
 		So(err, ShouldBeNil)
-		err = enforcer.sourcePortConnectionCache.Add(conn, tcpPacket.SourcePortHash(packet.PacketTypeApplication))
+		err = enforcer.sourcePortConnectionCache.Add(tcpPacket.SourcePortHash(packet.PacketTypeApplication), conn)
 		fmt.Println("Added", tcpPacket.SourcePortHash(packet.PacketTypeApplication))
 		So(err, ShouldBeNil)
 		_, err = enforcer.netSynAckRetrieveState(tcpSynAckPacket)
