6049cd6b6ddc7b4a5b08750d63aee6984d22f663
Mehul Chadha <mehul@aporeto.com>
2019-06-24 11:04:44 -0700

    Support services(TCP, HTTP) with ipv6 (#818)

---
+++ controller/internal/enforcer/applicationproxy/markedconn/markedconn_test.go
@@ -0,0 +1,128 @@
+// +build linux
+
+package markedconn
+
+import (
+	"context"
+	"encoding/binary"
+	"net"
+	"syscall"
+	"testing"
+	"unsafe"
+
+	"github.com/magiconair/properties/assert"
+)
+
+type testPassFD struct {
+}
+
+func (*testPassFD) Control(f func(uintptr)) error {
+	f(0)
+	return nil
+}
+
+func TestGetOrigDestV4(t *testing.T) {
+
+	testGetOrigV4 := func(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err syscall.Errno) {
+		sa := (*(*sockaddr4)(unsafe.Pointer(a4))) //nolint
+		sa.family = syscall.AF_INET
+		copy(sa.data[2:6], []byte{127, 0, 0, 1})
+		binary.BigEndian.PutUint16(sa.data[:2], 3000)
+		*(*sockaddr4)(unsafe.Pointer(a4)) = sa //nolint
+		return 0, 0, 0
+	}
+
+	test := &testPassFD{}
+	ip, port, _ := getOriginalDestInternal(test, true, testGetOrigV4)
+	assert.Equal(t, ip.String(), "127.0.0.1", "ip should be 127.0.0.1")
+	assert.Equal(t, port, 3000, "port should be 3000")
+}
+
+func TestGetOrigDestV6(t *testing.T) {
+	testGetOrigV6 := func(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err syscall.Errno) {
+		sa := (*(*sockaddr6)(unsafe.Pointer(a4))) //nolint
+		sa.family = syscall.AF_INET6
+
+		copy(sa.ip[:], net.ParseIP("::1"))
+		binary.BigEndian.PutUint16(sa.port[:], 3000)
+
+		*(*sockaddr6)(unsafe.Pointer(a4)) = sa //nolint
+		return 0, 0, 0
+	}
+
+	test := &testPassFD{}
+	ip, port, _ := getOriginalDestInternal(test, false, testGetOrigV6) //nolint
+	assert.Equal(t, ip.String(), "::1", "ip should be ::1")
+	assert.Equal(t, port, 3000, "port should be 3000")
+}
+
+func TestGetOrigDestV4Err1(t *testing.T) {
+
+	testGetOrigV4 := func(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err syscall.Errno) {
+		return 0, 0, 0
+	}
+
+	test := &testPassFD{}
+	_, _, err := getOriginalDestInternal(test, true, testGetOrigV4) //nolint
+
+	assert.Equal(t, err != nil, true, "error should not be nil")
+}
+
+func TestGetOrigDestV6Err1(t *testing.T) {
+	testGetOrigV6 := func(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err syscall.Errno) {
+		return 0, 0, 0
+	}
+
+	test := &testPassFD{}
+	_, _, err := getOriginalDestInternal(test, false, testGetOrigV6) //nolint
+	assert.Equal(t, err != nil, true, "error should not be nil")
+}
+
+func TestGetOrigDestV4Err2(t *testing.T) {
+
+	testGetOrigV4 := func(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err syscall.Errno) {
+		return 0, 0, 1
+	}
+
+	test := &testPassFD{}
+	_, _, err := getOriginalDestInternal(test, true, testGetOrigV4) //nolint
+	assert.Equal(t, err != nil, true, "error should not be nil")
+}
+
+func TestGetOrigDestV6Err2(t *testing.T) {
+	testGetOrigV6 := func(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err syscall.Errno) {
+		return 0, 0, 1
+	}
+
+	test := &testPassFD{}
+
+	_, _, err := getOriginalDestInternal(test, false, testGetOrigV6) //nolint
+	assert.Equal(t, err != nil, true, "error should not be nil")
+}
+
+func TestLocalAddr(t *testing.T) {
+	ip, _, _ := net.ParseCIDR("172.17.0.2/24")
+	proxyConn := ProxiedConnection{originalIP: ip, originalPort: 80}
+
+	naddr := proxyConn.LocalAddr()
+	netAddr := naddr.(*net.TCPAddr)
+
+	assert.Equal(t, ip, netAddr.IP, "ip should be equal to 172.17.0.2")
+	assert.Equal(t, 80, netAddr.Port, "ip should be equal to 172.17.0.2")
+}
+
+func TestSocketListener(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background()) //nolint
+	_, err := NewSocketListener(ctx, ":1111", 100)
+
+	assert.Equal(t, err, nil, "error  should be nil")
+	cancel() //nolint
+}
+
+func TestGetInterfaces(t *testing.T) {
+
+	ipmap := GetInterfaces()
+
+	b := len(ipmap) != 0
+	assert.Equal(t, b, true, "ipmap should not be empty")
+}
