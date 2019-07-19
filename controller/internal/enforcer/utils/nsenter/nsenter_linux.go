c6ef093d0968357dca4d0c023b55b09aab2c585e
Amit Limaye <amit@aporeto.com>
2019-07-19 15:37:22 -0700

    Undo var run

--- controller/internal/enforcer/utils/nsenter/nsenter_linux.go
+++ controller/internal/enforcer/utils/nsenter/nsenter_linux.go
@@ -15,11 +15,7 @@ extern void nsexec();
 extern void droppriveleges();
 extern void setupiptables();
 void __attribute__((constructor)) init(void) {
-        //Setup /var/run here this needs to be done here since /var/run will be created at start
-        int result = mkdir("/var/run/aporeto",0760);
-        if (result < 0) {
-           printf("Failed to create directory %s\n",strerror(errno));
-        }
+
 	nsexec();
         setupiptables();
         droppriveleges();
