10d534e13cce2563472bc62abff9e0df7dd97312
Amit Limaye <amit@aporeto.com>
2019-07-19 15:24:21 -0700

    Create var run

--- controller/internal/enforcer/utils/nsenter/nsenter_linux.go
+++ controller/internal/enforcer/utils/nsenter/nsenter_linux.go
@@ -6,11 +6,20 @@ package nsenter
 
 #cgo CFLAGS: -Wall
 #include<stdio.h>
-
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<errno.h>
+#include<string.h>
+extern int errno;
 extern void nsexec();
 extern void droppriveleges();
 extern void setupiptables();
 void __attribute__((constructor)) init(void) {
+        //Setup /var/run here this needs to be done here since /var/run will be created at start
+        int result = mkdir("/var/run/aporeto",0760);
+        if (result < 0) {
+           printf("Failed to create directory %s\n",strerror(errno));
+        }
 	nsexec();
         setupiptables();
         droppriveleges();
