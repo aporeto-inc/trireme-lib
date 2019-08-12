212cc477a68319951a936bfe68187abb68fa6832
Amit Limaye <amit@aporeto.com>
2019-08-12 14:54:50 -0700

    new code for monitor

---
+++ monitor/monitor_windows.go
@@ -0,0 +1,24 @@
+// +build !linux
+
+package monitor
+
+import (
+	"context"
+
+	"go.aporeto.io/trireme-lib/monitor/config"
+)
+
+// Run starts the monitor.
+func Run(ctx context.Context) error {
+	return nil
+}
+
+// UpdateConfiguration updates the configuration of the monitor
+func UpdateConfiguration(ctx context.Context, config *config.MonitorConfig) error {
+	return nil
+}
+
+// Resync requests to the monitor to do a resync.
+func Resync(ctx context.Context) error {
+	return nil
+}
