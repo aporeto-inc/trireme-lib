f6ddd2ffaf8ba5417f252f851216307125977490
mehul chadha <mehul@aporeto.com>
2019-08-21 17:03:45 -0700

    mock

--- controller/pkg/remoteenforcer/remoteenforcer_test.go
+++ controller/pkg/remoteenforcer/remoteenforcer_test.go
@@ -507,7 +507,7 @@ func TestInitEnforcer(t *testing.T) {
 				mockSupevisor.EXPECT().Run(server.ctx).Return(nil)
 				mockDebugClient.EXPECT().Run(server.ctx).Return(nil)
 				mockCounterClient.EXPECT().Run(server.ctx).Return(errors.New("failed to run counterclient"))
-				mockDNSReportClient.EXPECT().Run(server.ctx).Return(nil)
+				mockDNSReportClient.EXPECT().Run(server.ctx).Return(errors.New("failed to run mockdnsclient"))
 				mockSupevisor.EXPECT().CleanUp()
 				mockEnf.EXPECT().CleanUp()
 
