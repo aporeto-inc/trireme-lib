8e5283e00c25e553a62e8fe079cf2cd5100a1bd7
Philip Hamer <philip@aporeto.com>
2019-08-23 09:46:09 -0400

    correct arguments to frontman api call

--- controller/internal/enforcer/applicationproxy/markedconn/mark_windows.go
+++ controller/internal/enforcer/applicationproxy/markedconn/mark_windows.go
@@ -17,11 +17,7 @@ func makeDialer(mark int, nativeData *NativeData) net.Dialer {
 			return c.Control(func(fd uintptr) {
 
 				// call FrontmanApplyDestHandle to update WFP redirect data before the connect() call on the new socket
-				driverHandle, err := getDriverHandle()
-				if err != nil {
-					zap.L().Error(fmt.Sprintf("failed to get driver handle: %v", err))
-				}
-				dllRet, _, err := applyDestHandleProc.Call(driverHandle, fd, nativeData.handle)
+				dllRet, _, err := applyDestHandleProc.Call(fd, nativeData.handle)
 				if err != syscall.Errno(0) {
 					zap.L().Error(fmt.Sprintf("%s failed: %v", applyDestHandleProc.Name, err))
 				} else if dllRet == 0 {
