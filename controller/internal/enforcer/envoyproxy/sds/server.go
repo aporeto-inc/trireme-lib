945821fd2b1c027cc5f525b4abf89ce719ed6ef0
abhijitherekar <in.abhijit@gmail.com>
2019-09-03 21:34:33 -0700

    Add Run and NewServer API.

--- controller/internal/enforcer/envoyproxy/sds/server.go
+++ controller/internal/enforcer/envoyproxy/sds/server.go
@@ -10,7 +10,6 @@ import (
 
 	v2 "go.aporeto.io/trireme-lib/third_party/generated/envoyproxy/data-plane-api/envoy/api/v2"
 	sds "go.aporeto.io/trireme-lib/third_party/generated/envoyproxy/data-plane-api/envoy/service/discovery/v2"
-	"google.golang.org/grpc"
 )
 
 // Options to create a SDS server to task to envoy
@@ -22,6 +21,13 @@ type Options struct {
 type Server struct {
 	sdsGrpcServer   *grpc.Server
 	sdsGrpcListener net.Listener
+
+	errCh chan error
+}
+
+// NewServer creates a instance of a server.
+func NewServer() Server {
+	return Server{errCh: make(chan error)}
 }
 
 // CreateSdsService does the following
@@ -41,10 +47,17 @@ func (s *Server) CreateSdsService(options *Options) error { //nolint: unparam
 	return nil
 }
 
+// Run starts the sdsGrpcServer to serve
+func (s *Server) Run() {
+	go func() {
+		s.errCh <- s.sdsGrpcServer.Serve(s.sdsGrpcListener)
+	}()
+}
+
 // register adds the SDS handle to the grpc server
-func (s *Server) register(rpcs *grpc.Server) {
+func (s *Server) register(sdsGrpcServer *grpc.Server) {
 	fmt.Println("\n\n ** registering the secret discovery")
-	sds.RegisterSecretDiscoveryServiceServer(rpcs, s)
+	sds.RegisterSecretDiscoveryServiceServer(sdsGrpcServer, s)
 }
 
 // now implement the interfaces of the SDS grpc server.
@@ -61,12 +74,18 @@ func (s *Server) DeltaSecrets(stream sds.SecretDiscoveryService_DeltaSecretsServ
 
 // StreamSecrets is the function invoked by the envoy in-order to pull the certs, this also sends the response back to the envoy.
 // It does the following:
-// 1. create a rece
+// 1. create a receiver thread to stream.
+// 2. parse the discovery request.
+// 3. track the request.
+// 4. call the Aporeto api to generate the secret
 func (s *Server) StreamSecrets(stream sds.SecretDiscoveryService_StreamSecretsServer) error {
 	return nil
 }
 
 // FetchSecrets gets the discovery request and call the Aporeto backend to fetch the certs.
+// 1. parse the discovery request.
+// 2. track the request.
+// 3. call the Aporeto api to generate the secret
 func (s *Server) FetchSecrets(ctx context.Context, discReq *v2.DiscoveryRequest) (*v2.DiscoveryResponse, error) {
 	return nil, nil
 }
