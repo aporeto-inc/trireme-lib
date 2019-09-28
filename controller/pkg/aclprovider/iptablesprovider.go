219e1fed215886a4f3277b0495ac431a67dd1799
Sibi Chakravarthy <sibiitdeveloper@gmail.com>
2019-09-27 17:45:52 -0700

    fix: support quoted rules

--- controller/pkg/aclprovider/iptablesprovider.go
+++ controller/pkg/aclprovider/iptablesprovider.go
@@ -53,6 +53,7 @@ type BatchProvider struct {
 	commitFunc func(buf *bytes.Buffer) error
 	sync.Mutex
 	restoreCmd string
+	quote      bool
 }
 
 const (
@@ -72,17 +73,18 @@ func NewGoIPTablesProviderV4(batchTables []string) (*BatchProvider, error) {
 	// We will only support the batch method if there is iptables-restore and iptables
 	// version 1.6.2 or better. Otherwise, we fall back to classic iptables instructions.
 	// This will allow us to support older kernel versions.
-	if restoreHasWait(restoreCmdV4) {
-		for _, t := range batchTables {
-			batchTablesMap[t] = true
-		}
-	}
+	// if restoreHasWait(restoreCmdV4) {
+	// 	for _, t := range batchTables {
+	// 		batchTablesMap[t] = true
+	// 	}
+	// }
 
 	b := &BatchProvider{
 		ipt:         ipt,
 		rules:       map[string]map[string][]string{},
 		batchTables: batchTablesMap,
 		restoreCmd:  restoreCmdV4,
+		quote:       true,
 	}
 
 	b.commitFunc = b.restore
@@ -113,6 +115,7 @@ func NewGoIPTablesProviderV6(batchTables []string) (*BatchProvider, error) {
 		rules:       map[string]map[string][]string{},
 		batchTables: batchTablesMap,
 		restoreCmd:  restoreCmdV6,
+		quote:       true,
 	}
 
 	b.commitFunc = b.restore
@@ -157,6 +160,8 @@ func (b *BatchProvider) Append(table, chain string, rulespec ...string) error {
 		b.rules[table][chain] = []string{}
 	}
 
+	b.quoteRulesSpec(rulespec)
+
 	rule := strings.Join(rulespec, " ")
 	b.rules[table][chain] = append(b.rules[table][chain], rule)
 	return nil
@@ -180,6 +185,8 @@ func (b *BatchProvider) Insert(table, chain string, pos int, rulespec ...string)
 	if _, ok := b.rules[table][chain]; !ok {
 		b.rules[table][chain] = []string{}
 	}
+
+	b.quoteRulesSpec(rulespec)
 
 	rule := strings.Join(rulespec, " ")
 
@@ -212,6 +219,8 @@ func (b *BatchProvider) Delete(table, chain string, rulespec ...string) error {
 	if _, ok := b.rules[table][chain]; !ok {
 		return nil
 	}
+
+	b.quoteRulesSpec(rulespec)
 
 	rule := strings.Join(rulespec, " ")
 	for index, r := range b.rules[table][chain] {
@@ -369,6 +378,17 @@ func (b *BatchProvider) restore(buf *bytes.Buffer) error {
 		return fmt.Errorf("Failed to execute iptables-restore: %s", err)
 	}
 	return nil
+}
+
+func (b *BatchProvider) quoteRulesSpec(rulesspec []string) {
+
+	if !b.quote {
+		return
+	}
+
+	for i, rule := range rulesspec {
+		rulesspec[i] = fmt.Sprintf("\"%s\"", rule)
+	}
 }
 
 func restoreHasWait(restoreCmd string) bool {
