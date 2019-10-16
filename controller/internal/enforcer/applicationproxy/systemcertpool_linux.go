65166e905435ae3e20a9d70dda1060f83ebd5fdc
Philip Hamer <philip@aporeto.com>
2019-10-16 16:18:27 -0400

    systemcertpool code cleanup

--- controller/internal/enforcer/applicationproxy/systemcertpool_linux.go
+++ controller/internal/enforcer/applicationproxy/systemcertpool_linux.go
@@ -1,15 +1,11 @@
-// +build linux
+// +build !windows
 
 package applicationproxy
 
 import (
 	"crypto/x509"
-
-	"go.uber.org/zap"
 )
 
 func GetSystemCertPool() (*x509.CertPool, error) {
-	zap.L().Error("LInux Code")
 	return x509.SystemCertPool()
-
 }
--- controller/internal/enforcer/applicationproxy/systemcertpool_windows.go
+++ controller/internal/enforcer/applicationproxy/systemcertpool_windows.go
@@ -1,4 +1,4 @@
-// +build windows !linux
+// +build windows
 
 package applicationproxy
 
