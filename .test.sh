f196344305ae0a2af7350a3164582f2f332aa28d
Brian Onn <brian@aporeto.com>
2019-10-31 16:25:29 -0700

    added test delineator msg

--- .test.sh
+++ .test.sh
@@ -2,17 +2,25 @@
 
 export GO111MODULE=on
 
-set -e
-echo "" > coverage.txt
+# set -e
+echo >| coverage.txt
 
 # temporarily for debugging travis build
 # how did this even work all this time? '-source_package' is not an option for a long time
 #./mockgen.sh
 
-for d in $(go list ./... | grep -v mock); do
-    go test -race -tags test -coverprofile=profile.out -covermode=atomic $d
+echo
+echo  "========= BEGIN TESTS ==========="
+echo
+
+for d in $(go list ./... | grep -E -v '/mock[^/]+$' ); do
+    go test -v -race -tags test -coverprofile=profile.out -covermode=atomic "$d"
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
 done
+
+echo
+echo  "========= END TESTS ==========="
+echo
