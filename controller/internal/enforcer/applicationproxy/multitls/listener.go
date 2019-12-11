610421ab59a62855f8a5343d65c25554adff8bb2
Marcus Heese <marcus@aporeto.com>
2019-12-11 10:03:18 -0800

    simplifying multi tls

--- controller/internal/enforcer/applicationproxy/multitls/listener.go
+++ controller/internal/enforcer/applicationproxy/multitls/listener.go
@@ -6,29 +6,22 @@ package multitls
 import (
 	"crypto/tls"
 	"errors"
-	"fmt"
 	"net"
 
 	"go.aporeto.io/trireme-lib/controller/internal/enforcer/applicationproxy/markedconn"
-	"go.aporeto.io/trireme-lib/controller/internal/enforcer/applicationproxy/serviceregistry"
 )
 
 // ensure that Listener implements net.Listener
 var _ net.Listener = &Listener{}
 
-var (
-	// InvalidListenerErr will be returned in NewMultiTLSListener if the provided listener is not a ProxiedListener
-	InvalidListenerErr = errors.New("multitls: invalid listener, listener must be of type ProxiedListener")
-
-	// InvalidConnErr will be returned when the accepted connection is not a ProxiedConnection
-	InvalidConnErr = errors.New("multitls: invalid connection, connection must be of type ProxiedConnection")
-)
+// InvalidConnErr will be returned when the accepted connection is not a ProxiedConnection
+var InvalidConnErr = errors.New("multitls: invalid connection, connection must be of type ProxiedConnection")
 
 type Listener struct {
 	net.Listener
-	registry *serviceregistry.Registry
-	internal *tls.Config
-	public   *tls.Config
+	publicPort int
+	internal   *tls.Config
+	public     *tls.Config
 }
 
 // Accept waits for and returns the next connection to the listener.
@@ -42,22 +35,9 @@ func (l *Listener) Accept() (net.Conn, error) {
 		return nil, InvalidConnErr
 	}
 
-	ip, port := mc.GetOriginalDestination()
-	pc, err := l.registry.RetrieveExposedServiceContext(ip, port, "")
-	if err != nil {
-		return nil, fmt.Errorf("multitls: unknown service: %s", err)
-	}
-
-	if pc.Service == nil {
-		return nil, fmt.Errorf("multitls: empty service")
-	}
-
-	if pc.Service.PublicNetworkInfo != nil && pc.Service.PublicNetworkInfo.Ports != nil {
-		publicPort, err := pc.Service.PublicNetworkInfo.Ports.SinglePort()
-		if err != nil {
-			return nil, fmt.Errorf("multitls: service public port: %s", err)
-		}
-		if publicPort == uint16(port) {
+	if l.publicPort > 0 {
+		_, port := mc.GetOriginalDestination()
+		if l.publicPort == port {
 			return tls.Server(mc, l.public), nil
 		}
 	}
@@ -67,15 +47,11 @@ func (l *Listener) Accept() (net.Conn, error) {
 }
 
 // NewMultiTLSListener will create a new multi TLS listener.
-func NewMultiTLSListener(l net.Listener, registry *serviceregistry.Registry, internal, public *tls.Config) (net.Listener, error) {
-	if _, ok := l.(*markedconn.ProxiedListener); !ok {
-		return nil, InvalidListenerErr
-	}
-
+func NewMultiTLSListener(l net.Listener, publicPort int, internal, public *tls.Config) *Listener {
 	return &Listener{
-		Listener: l,
-		registry: registry,
-		internal: internal,
-		public:   public,
-	}, nil
+		Listener:   l,
+		publicPort: publicPort,
+		internal:   internal,
+		public:     public,
+	}
 }
