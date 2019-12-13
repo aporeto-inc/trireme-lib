f4df395db2f103c868e6284cd2a7c2130ec9b341
Philip Hamer <philip@aporeto.com>
2019-12-13 17:31:21 -0500

    add comment about change in proxy Accept behavior, per code review

--- controller/internal/enforcer/applicationproxy/protomux/protomux.go
+++ controller/internal/enforcer/applicationproxy/protomux/protomux.go
@@ -184,6 +184,8 @@ func (m *MultiplexedListener) Serve(ctx context.Context) error {
 					return nil
 				default:
 				}
+				// if it is an actual error (which can happen in Windows we can't get origin ip/port from our driver),
+				// then log an error and continue accepting connections.
 				zap.L().Error("error from Accept", zap.Error(err))
 				break
 			}
