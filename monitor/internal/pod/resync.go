03621d579447807dfdff060986715cf03e247023
Marcus Heese <marcus@aporeto.com>
2020-01-22 16:39:05 -0800

    making Resync a blocked operation for the PodMonitor and calling it on startup

--- monitor/internal/pod/resync.go
+++ monitor/internal/pod/resync.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"sync"
 
 	corev1 "k8s.io/api/core/v1"
 
@@ -14,8 +15,9 @@ import (
 )
 
 // ResyncWithAllPods is called from the implemented resync, it will list all pods
-// and fire them down the event source (the generic event channel)
-func ResyncWithAllPods(ctx context.Context, c client.Client, evCh chan<- event.GenericEvent) error {
+// and fire them down the event source (the generic event channel).
+// It will block until every pod at the time of calling has been calling `Reconcile` at least once.
+func ResyncWithAllPods(ctx context.Context, c client.Client, i *ResyncInfoChan, evCh chan<- event.GenericEvent) error {
 	if c == nil {
 		return errors.New("pod: no client available")
 	}
@@ -24,11 +26,29 @@ func ResyncWithAllPods(ctx context.Context, c client.Client, evCh chan<- event.G
 		return errors.New("pod: no event source available")
 	}
 
+	if i == nil {
+		return errors.New("pod: no resync info channel available")
+	}
+
 	list := &corev1.PodList{}
 	if err := c.List(ctx, &client.ListOptions{}, list); err != nil {
 		return fmt.Errorf("pod: %s", err.Error())
 	}
 
+	// build a map of pods that we will expect to turn true
+	m := make(map[string]bool)
+	for _, pod := range list.Items {
+		podName := pod.GetName()
+		podNamespace := pod.GetNamespace()
+		if podName != "" && podNamespace != "" {
+			m[fmt.Sprintf("%s/%s", podNamespace, podName)] = false
+		}
+	}
+
+	// Request that the controller reports to us from now on
+	i.EnableNeedsInfo()
+
+	// fire away events to the controller
 	for _, pod := range list.Items {
 		p := pod.DeepCopy()
 		evCh <- event.GenericEvent{
@@ -37,5 +57,75 @@ func ResyncWithAllPods(ctx context.Context, c client.Client, evCh chan<- event.G
 		}
 	}
 
+	// now wait for all pods to have reported back
+waitLoop:
+	for {
+		info := <-*i.GetInfoCh()
+		if _, ok := m[info]; ok {
+			m[info] = true
+		}
+
+		// now check if we can abort already
+		for _, v := range m {
+			if !v {
+				continue waitLoop
+			}
+		}
+		break waitLoop
+	}
+	i.DisableNeedsInfo()
+
 	return nil
+}
+
+// ResyncInfoChan is used to report back from the controller on which pods it has processed.
+// It allows the Resync of the monitor to block and wait until a list has been processed.
+type ResyncInfoChan struct {
+	m  sync.RWMutex
+	b  bool
+	ch chan string
+}
+
+// NewResyncInfoChan creates a new ResyncInfoChan
+func NewResyncInfoChan() *ResyncInfoChan {
+	return &ResyncInfoChan{
+		ch: make(chan string, 100),
+	}
+}
+
+// EnableNeedsInfo enables the need for sending info
+func (r *ResyncInfoChan) EnableNeedsInfo() {
+	r.m.Lock()
+	defer r.m.Unlock()
+	r.b = true
+}
+
+// DisableNeedsInfo disables the need for sending info
+func (r *ResyncInfoChan) DisableNeedsInfo() {
+	r.m.Lock()
+	defer r.m.Unlock()
+	r.b = false
+}
+
+// NeedsInfo returns if there is a need for sending info
+func (r *ResyncInfoChan) NeedsInfo() bool {
+	r.m.RLock()
+	defer r.m.RUnlock()
+	return r.b
+}
+
+// SendInfo will make the info available through an internal channel
+func (r *ResyncInfoChan) SendInfo(info string) {
+	r.m.RLock()
+	defer r.m.RUnlock()
+	if r.b {
+		r.ch <- info
+	}
+}
+
+// GetInfoCh returns the channel
+func (r *ResyncInfoChan) GetInfoCh() *chan string {
+	r.m.RLock()
+	defer r.m.RUnlock()
+	return &r.ch
 }
