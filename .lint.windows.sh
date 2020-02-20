85e38e2d1fc105dfe56cd370fffc54f5eeb6a5e1
Philip Hamer <philip@aporeto.com>
2020-02-20 18:33:57 -0500

    lint windows script

---
+++ .lint.windows.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 golangci-lint run --deadline=10m --disable-all --exclude-use-default=false --enable=errcheck --enable=goimports --enable=ineffassign --enable=govet --enable=golint --enable=unused --enable=structcheck --enable=varcheck --enable=deadcode --enable=unconvert --enable=goconst --enable=gosimple --enable=misspell --enable=staticcheck --enable=unparam --enable=prealloc --enable=nakedret --enable=gofmt --enable=typecheck --skip-dirs=vendor/github.com/iovisor ./...
