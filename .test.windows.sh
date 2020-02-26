0e55566c13f25d32720c74c7218964cb86248c4a
Philip Hamer <philip@aporeto.com>
2020-02-26 17:43:28 -0500

    windows unit tests - exclude pam

--- .test.windows.sh
+++ .test.windows.sh
@@ -3,7 +3,7 @@
 set -e
 echo "" > coverage.windows.txt
 
-for d in $(go list ./... | grep -v remoteenforcer | grep -v remoteapi); do
+for d in $(go list ./... | grep -v remoteenforcer | grep -v remoteapi | grep -v "plugins/pam"); do
     GOOS=windows GOARCH=amd64 go test -tags test -exec wine -coverprofile=profile.windows.out -covermode=atomic $d
     if [ -f profile.windows.out ]; then
         cat profile.windows.out >> coverage.windows.txt
