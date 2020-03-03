6ec1532d06121387964e37e0d02d4291c9b7cf0c
Philip Hamer <philip@aporeto.com>
2020-03-03 15:37:01 -0500

    try windows tests without coverage

--- .test.windows.sh
+++ .test.windows.sh
@@ -9,12 +9,12 @@ else
 fi
 
 set -e
-echo "" > coverage.windows.txt
+#echo "" > coverage.windows.txt
 
 for d in $(CGO_ENABLED=0 go list ./... | grep -v remoteenforcer | grep -v remoteapi | grep -v "plugins/pam"); do
-    CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go test -tags test $WINE_EXEC -coverprofile=profile.windows.out -covermode=atomic $d
-    if [ -f profile.windows.out ]; then
-        cat profile.windows.out >> coverage.windows.txt
-        rm profile.windows.out
-    fi
+    CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go test -tags test $WINE_EXEC $d
+    #if [ -f profile.windows.out ]; then
+    #    cat profile.windows.out >> coverage.windows.txt
+    #    rm profile.windows.out
+    #fi
 done
