7ff95ded77ab96151440c55b9ea4d115c89ab40f
Amit Limaye <alimaye@paloaltonetworks.com>
2020-03-05 14:53:55 -0800

    Add an indexallocator

---
+++ utils/indexallocator/indexallocator.go
@@ -0,0 +1,41 @@
+package indexallocator
+
+import "sync"
+
+type allocator struct {
+	size         int
+	startIndex   int
+	indexChannel chan int
+}
+
+var indexes *allocator = &allocator{
+	size:       0,
+	startIndex: 0,
+}
+var once sync.Once
+
+// New create a new indexallocator
+func New(size int, startIndex int) (IndexAllocator, int, int) {
+	if indexes.size != 0 {
+		return indexes, indexes.size, indexes.startIndex
+	}
+	once.Do(func() {
+		indexes = &allocator{
+			size:         size,
+			startIndex:   startIndex,
+			indexChannel: make(chan int, size),
+		}
+		for i := startIndex; i < startIndex+size; i++ {
+			indexes.indexChannel <- i
+		}
+	})
+	return indexes, size, startIndex
+}
+
+func (i *allocator) Get() int {
+	return <-i.indexChannel
+}
+
+func (i *allocator) Put(index int) {
+	i.indexChannel <- index
+}
