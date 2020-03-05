5f8e971f2030541259f2faf25644d3ee3d073dbb
Brian Onn <brian@aporeto.com>
2020-03-04 21:11:49 -0800

    disable ptr checking for go1.14 only

--- .test.sh
+++ .test.sh
@@ -8,16 +8,19 @@ echo "" > coverage.txt
 
 ## FIX ME. go1.14 automatically enables unsafe ptr checks when doing race checks,
 ## and it is not clear if this is compatible (it is disabled on Windows)
+##
+## This needs to be revisited and maybe remove "-gcflags=all=-d=checkptr=0" below
+## for go1.14 once we determine if there is a real pointer issue in the tests.
+##
+## this is the file that fails when ptr checking is enabled:
+## go.aporeto.io/trireme-lib/controller/internal/enforcer/applicationproxy/markedconn_test.go
+##
+## to see the failure, test that package individually setting "checkptr=1"
 
-## this needs to be revisited and maybe remove "-gcflags=all=-d=checkptr=0" below
-## once we determine if there is a real pointer issue in the tests. 
-
-## this package fails when ptr checking is enabled:
-## go.aporeto.io/trireme-lib/controller/internal/enforcer/applicationproxy/markedconn  
-
-## to see the failure, test that package individually with "checkptr=1"
-
-CHECKPTR="-gcflags=all=-d=checkptr=0"
+case "$(go version)" in
+    *1.13*) CHECKPTR=""  ;;
+    *)      CHECKPTR="-gcflags=all=-d=checkptr=0" ;;
+esac
 
 for d in $(go list ./... | grep -v 'mock|bpf'); do
     go test ${CHECKPTR} -race -tags test -coverprofile=profile.out -covermode=atomic $d
