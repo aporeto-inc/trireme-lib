43962c08fb3ef454cc9cfa508f177af02bb8c456
Marcus Heese <marcus@aporeto.com>
2020-03-04 20:00:07 -0800

    adding unit test for controller startup

---
+++ monitor/internal/pod/monitor_test.go
@@ -0,0 +1,162 @@
+// +build linux !windows
+
+package podmonitor
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func TestPodMonitor_startManager(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mgr := NewMockManager(ctrl)
+	c := NewMockClient(ctrl)
+	zc := NewMockCore(ctrl)
+	zc.EXPECT().Enabled(zapcore.DebugLevel).AnyTimes().Return(false)
+
+	logger := zap.New(zc)
+	zap.ReplaceGlobals(logger)
+
+	ctx := context.Background()
+	ctxWithCancel, cancel := context.WithCancel(context.Background())
+
+	type args struct {
+		ctx context.Context
+		mgr manager.Manager
+	}
+
+	m := New()
+	tests := []struct {
+		name           string
+		m              *PodMonitor
+		args           args
+		expect         func(t *testing.T)
+		wantErr        bool
+		wantKubeClient bool
+	}{
+		{
+			name: "successful startup",
+			m:    m,
+			args: args{
+				ctx: ctx,
+				mgr: mgr,
+			},
+			wantErr:        false,
+			wantKubeClient: true,
+			expect: func(t *testing.T) {
+				var r manager.Runnable
+				mgr.EXPECT().Add(gomock.Any()).DoAndReturn(func(run manager.Runnable) error {
+					r = run
+					return nil
+				}).Times(1)
+				mgr.EXPECT().Start(gomock.Any()).DoAndReturn(func(z <-chan struct{}) error {
+					go r.Start(z)
+					return nil
+				}).Times(1)
+				mgr.EXPECT().GetClient().Times(1).Return(c)
+			},
+		},
+		{
+			name: "successful startup after 6s must write warning log",
+			m:    m,
+			args: args{
+				ctx: ctx,
+				mgr: mgr,
+			},
+			wantErr:        false,
+			wantKubeClient: true,
+			expect: func(t *testing.T) {
+				zc.EXPECT().Enabled(zapcore.WarnLevel).Times(1).Return(true)
+				zc.EXPECT().Check(gomock.Any(), gomock.Any()).Times(1).DoAndReturn(func(ent zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.CheckedEntry {
+					return ce.AddCore(ent, zc)
+				})
+				zc.EXPECT().Write(gomock.Any(), gomock.Any()).Times(1).DoAndReturn(func(ent zapcore.Entry, fields []zapcore.Field) error {
+					expectedLogMessage := "pod: controller did not start within 5s"
+					if ent.Message != expectedLogMessage {
+						t.Errorf("expectedLogMessage = '%s', ent.Message = '%s'", expectedLogMessage, ent.Message)
+					}
+					return nil
+				})
+				var r manager.Runnable
+				mgr.EXPECT().Add(gomock.Any()).DoAndReturn(func(run manager.Runnable) error {
+					r = run
+					return nil
+				}).Times(1)
+				mgr.EXPECT().Start(gomock.Any()).DoAndReturn(func(z <-chan struct{}) error {
+					go func() {
+						time.Sleep(6 * time.Second)
+						r.Start(z)
+					}()
+					return nil
+				}).Times(1)
+				mgr.EXPECT().GetClient().Times(1).Return(c)
+			},
+		},
+		{
+			name: "adding controller fails",
+			m:    m,
+			args: args{
+				ctx: ctx,
+				mgr: mgr,
+			},
+			wantErr: true,
+			expect: func(t *testing.T) {
+				mgr.EXPECT().Add(gomock.Any()).Return(fmt.Errorf("error")).Times(1)
+			},
+		},
+		{
+			name: "starting controller fails",
+			m:    m,
+			args: args{
+				ctx: ctx,
+				mgr: mgr,
+			},
+			wantErr: true,
+			expect: func(t *testing.T) {
+				mgr.EXPECT().Add(gomock.Any()).Return(nil).Times(1)
+				mgr.EXPECT().Start(gomock.Any()).Return(fmt.Errorf("error")).Times(1)
+			},
+		},
+		{
+			name: "context is being cancelled cancelled",
+			m:    m,
+			args: args{
+				ctx: ctxWithCancel,
+				mgr: mgr,
+			},
+			wantErr: true,
+			expect: func(t *testing.T) {
+				mgr.EXPECT().Add(gomock.Any()).Return(nil).Times(1)
+				mgr.EXPECT().Start(gomock.Any()).DoAndReturn(func(z <-chan struct{}) error {
+					cancel()
+					return nil
+				}).Times(1)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.expect(t)
+			tt.m.kubeClient = nil
+			if err := tt.m.startManager(tt.args.ctx, tt.args.mgr); (err != nil) != tt.wantErr {
+				t.Errorf("PodMonitor.startManager() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantKubeClient && tt.m.kubeClient == nil {
+				t.Errorf("PodMonitor.startManager() kubeClient = %v, wantKubeClient %v", tt.m.kubeClient, tt.wantKubeClient)
+			}
+			if !tt.wantKubeClient && tt.m.kubeClient != nil {
+				t.Errorf("PodMonitor.startManager() kubeClient = %v, wantKubeClient %v", tt.m.kubeClient, tt.wantKubeClient)
+			}
+		})
+	}
+}
