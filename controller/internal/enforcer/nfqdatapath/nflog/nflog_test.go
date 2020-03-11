1912aa35f5f27715d2502c14781ac6b556b43f10
Sibi Chakravarthy Ramesh <sibiitdeveloper@gmail.com>
2020-03-10 18:12:12 -0700

    fix: unit test

--- controller/internal/enforcer/nfqdatapath/nflog/nflog_test.go
+++ controller/internal/enforcer/nfqdatapath/nflog/nflog_test.go
@@ -10,6 +10,7 @@ import (
 	"go.aporeto.io/netlink-go/nflog"
 	"go.aporeto.io/trireme-lib/common"
 	"go.aporeto.io/trireme-lib/controller/internal/enforcer/utils/packetgen"
+	"go.aporeto.io/trireme-lib/controller/pkg/counters"
 	"go.aporeto.io/trireme-lib/controller/pkg/packet"
 	"go.aporeto.io/trireme-lib/controller/pkg/pucontext"
 	"go.aporeto.io/trireme-lib/policy"
@@ -122,5 +123,60 @@ func TestRecordFromNFLogBuffer(t *testing.T) {
 
 		})
 
+	})
+}
+
+func Test_RecordCounters(t *testing.T) {
+	Convey("I report a dropped packet", t, func() {
+		puID := "SomeProcessingUnitId"
+		puInfo := policy.NewPUInfo(puID, "/ns", common.ContainerPU)
+		pu, err := pucontext.NewPU("contextID", puInfo, 5*time.Second)
+		So(err, ShouldBeNil)
+
+		Convey("I call record counters", func() {
+			recordCounters(6, 80, 2333, pu, true)
+			So(pu.Counters().GetErrorCounters()[counters.ErrDroppedTCPPackets], ShouldEqual, 1)
+
+			recordCounters(17, 80, 2333, pu, true)
+			c := pu.Counters().GetErrorCounters()
+			So(c[counters.ErrDroppedUDPPackets], ShouldEqual, 1)
+			recordCounters(17, 53, 2333, pu, true)
+			c = pu.Counters().GetErrorCounters()
+			So(c[counters.ErrDroppedUDPPackets], ShouldEqual, 1)
+			So(c[counters.ErrDroppedDNSPackets], ShouldEqual, 1)
+			recordCounters(17, 67, 2333, pu, true)
+			c = pu.Counters().GetErrorCounters()
+			So(c[counters.ErrDroppedUDPPackets], ShouldEqual, 1)
+			So(c[counters.ErrDroppedDHCPPackets], ShouldEqual, 1)
+			recordCounters(17, 68, 2333, pu, true)
+			c = pu.Counters().GetErrorCounters()
+			So(c[counters.ErrDroppedUDPPackets], ShouldEqual, 1)
+			So(c[counters.ErrDroppedDHCPPackets], ShouldEqual, 1)
+			recordCounters(17, 123, 2333, pu, true)
+			c = pu.Counters().GetErrorCounters()
+			So(c[counters.ErrDroppedUDPPackets], ShouldEqual, 1)
+			So(c[counters.ErrDroppedNTPPackets], ShouldEqual, 1)
+
+			recordCounters(17, 2333, 53, pu, false)
+			c = pu.Counters().GetErrorCounters()
+			So(c[counters.ErrDroppedUDPPackets], ShouldEqual, 1)
+			So(c[counters.ErrDroppedDNSPackets], ShouldEqual, 1)
+			recordCounters(17, 2333, 67, pu, false)
+			recordCounters(17, 2333, 67, pu, false)
+			c = pu.Counters().GetErrorCounters()
+			So(c[counters.ErrDroppedUDPPackets], ShouldEqual, 2)
+			So(c[counters.ErrDroppedDHCPPackets], ShouldEqual, 2)
+			recordCounters(17, 2333, 68, pu, false)
+			c = pu.Counters().GetErrorCounters()
+			So(c[counters.ErrDroppedUDPPackets], ShouldEqual, 1)
+			So(c[counters.ErrDroppedDHCPPackets], ShouldEqual, 1)
+			recordCounters(17, 2333, 123, pu, false)
+			c = pu.Counters().GetErrorCounters()
+			So(c[counters.ErrDroppedUDPPackets], ShouldEqual, 1)
+			So(c[counters.ErrDroppedNTPPackets], ShouldEqual, 1)
+
+			recordCounters(1, 80, 2333, pu, true)
+			So(pu.Counters().GetErrorCounters()[counters.ErrDroppedICMPPackets], ShouldEqual, 1)
+		})
 	})
 }
