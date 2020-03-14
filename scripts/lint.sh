616bd1755674ee2515e7f31c46404bbb7203d11f
Brian Onn <brian@aporeto.com>
2020-03-14 04:32:42 -0700

    fixed the linter

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-#
-# linter has trouble with go modules cache.
-# so we run it in a local vendor dir mode, but we don't
-# lint the vendor files.  Just need to load them so the linter can find them
-#
-GO111MODULE=auto go mod vendor
 GO111MODULE=auto golangci-lint run \
     --verbose \
-    --modules-download-mode=vendor \
     --skip-dirs='[third_party|test|vendor]' \
     --skip-files='[.*\.pb\.go|.*\.gen\.go]' \
     --deadline=20m \
