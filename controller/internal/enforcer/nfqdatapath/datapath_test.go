2d208013c6427be3286ad4c44a775b41417327f1
Amit Limaye <alimaye@paloaltonetworks.com>
2020-03-17 08:26:08 -0700

    Fix uni test

--- controller/internal/enforcer/nfqdatapath/datapath_test.go
+++ controller/internal/enforcer/nfqdatapath/datapath_test.go
@@ -1,5 +1,3 @@
-// +build !windows
-
 package nfqdatapath
 
 import (
@@ -9,6 +7,7 @@ import (
 	"math/rand"
 	"net"
 	"reflect"
+	"strconv"
 	"testing"
 	"time"
 
@@ -36,6 +35,7 @@ import (
 	"go.aporeto.io/trireme-lib/controller/pkg/tokens"
 	"go.aporeto.io/trireme-lib/controller/runtime"
 	"go.aporeto.io/trireme-lib/policy"
+	markconstants "go.aporeto.io/trireme-lib/utils/constants"
 	"go.aporeto.io/trireme-lib/utils/portspec"
 )
 
@@ -1458,7 +1458,8 @@ func TestContextFromIP(t *testing.T) {
 			enforcer.mode = constants.LocalServer
 
 			Convey("If the mark exists", func() {
-				ctx, err := enforcer.contextFromIP(true, "100", 0, packet.IPProtocolTCP)
+				markVal := strconv.Itoa(100 << markconstants.MarkShift)
+				ctx, err := enforcer.contextFromIP(true, markVal, 0, packet.IPProtocolTCP)
 				So(err, ShouldBeNil)
 				So(ctx, ShouldNotBeNil)
 				So(ctx, ShouldEqual, context)
