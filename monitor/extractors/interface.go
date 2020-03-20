32d77615be3f2b27c2bba088ff139f220deaeeda
Marcus Heese <marcus@aporeto.com>
2020-03-19 18:57:34 -0700

    slimming pod extractor interface

--- monitor/extractors/interface.go
+++ monitor/extractors/interface.go
@@ -7,9 +7,6 @@ import (
 	"go.aporeto.io/trireme-lib/policy"
 
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // EventMetadataExtractor is a function used to extract a *policy.PURuntime from a given
@@ -19,7 +16,7 @@ type EventMetadataExtractor func(*common.EventInfo) (*policy.PURuntime, error)
 // PodMetadataExtractor is a function used to extract a *policy.PURuntime from a given
 // Kubernetes pod. It can furthermore extract more information using the client.
 // The 5th argument (bool) indicates if a network namespace should get extracted
-type PodMetadataExtractor func(context.Context, client.Client, *runtime.Scheme, *corev1.Pod, bool) (*policy.PURuntime, error)
+type PodMetadataExtractor func(context.Context, *corev1.Pod, bool) (*policy.PURuntime, error)
 
 // PodSandboxExtractor is a function used to extract the SandboxID from a given pod.
 type PodSandboxExtractor func(context.Context, *corev1.Pod) (string, error)
