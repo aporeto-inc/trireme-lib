77bab191027dbc8154e0efea92f8c911d716c167
amitlimaye <amit.limaye@gmail.com>
2020-03-23 09:48:31 -0700

    nfqueue balance (#1007)
    Co-authored-by: Amit Limaye <alimaye@paloaltonetworks.com>

--- utils/cgnetcls/cgnetcls_windows.go
+++ utils/cgnetcls/cgnetcls_windows.go
@@ -2,7 +2,16 @@
 
 package cgnetcls
 
-import "sync/atomic"
+import (
+	"sync/atomic"
+
+	"go.aporeto.io/trireme-lib/utils/constants"
+)
+
+type netCls struct {
+}
+
+var markval uint64 = constants.Initialmarkval
 
 // MarkVal returns a new Mark Value
 func MarkVal() uint64 {
