f0a17d1394c3f3f929fab3823068c38444bf2a81
David Tillery <dtillery@paloaltonetworks.com>
2020-08-25 19:18:52 -0400

    Added port range support

--- controller/pkg/pucontext/pucontext_test.go
+++ controller/pkg/pucontext/pucontext_test.go
@@ -7,7 +7,9 @@ import (
 	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
+	"go.aporeto.io/trireme-lib/controller/constants"
 	"go.aporeto.io/trireme-lib/policy"
+	"go.aporeto.io/trireme-lib/utils/portspec"
 )
 
 func Test_NewPU(t *testing.T) {
@@ -27,5 +29,130 @@ func Test_NewPU(t *testing.T) {
 			So(pu.Counters(), ShouldNotBeNil)
 			So(err, ShouldBeNil)
 		})
+	})
+}
+
+func Test_PUSearch(t *testing.T) {
+
+	Convey("When I call PU Search", t, func() {
+
+		portRange80, _ := portspec.NewPortSpec(80, 85, nil)
+		portRange90, _ := portspec.NewPortSpec(90, 100, nil)
+
+		tagSelectorList := policy.TagSelectorList{
+			policy.TagSelector{
+				Clause: []policy.KeyValueOperator{
+					{
+						Key:      "app",
+						Value:    []string{"web"},
+						ID:       "asfasfasdasd",
+						Operator: policy.Equal,
+					},
+					{
+						Key:       "@sys:port",
+						Value:     []string{"TCP"},
+						ID:        "",
+						Operator:  policy.Equal,
+						PortRange: portRange80,
+					},
+				},
+				Policy: &policy.FlowPolicy{
+					PolicyID: "2",
+					Action:   policy.Accept,
+				},
+			},
+			policy.TagSelector{
+				Clause: []policy.KeyValueOperator{
+					{
+						Key:      "app",
+						Value:    []string{"web"},
+						ID:       "asfasfasdasd",
+						Operator: policy.Equal,
+					},
+					{
+						Key:       "@sys:port",
+						Value:     []string{"TCP"},
+						ID:        "",
+						Operator:  policy.Equal,
+						PortRange: portRange90,
+					},
+				},
+				Policy: &policy.FlowPolicy{
+					PolicyID: "2",
+					Action:   policy.Accept,
+				},
+			},
+		}
+
+		d := policy.NewPUPolicy(
+			"id",
+			"/abc",
+			policy.AllowAll,
+			nil,
+			nil,
+			nil,
+			nil,
+			tagSelectorList,
+			nil,
+			nil,
+			nil,
+			nil,
+			0,
+			0,
+			nil,
+			nil,
+			[]string{},
+			policy.EnforcerMapping,
+		)
+
+		fp := &policy.PUInfo{
+			Runtime: policy.NewPURuntimeWithDefaults(),
+			Policy:  d,
+		}
+
+		pu, _ := NewPU("pu1", fp, 24*time.Hour)
+
+		tags := policy.NewTagStore()
+		tags.AppendKeyValue("app", "web")
+		tags.AppendKeyValue(constants.PortNumberLabelString, "TCP/85")
+
+		report, flow := pu.SearchRcvRules(tags)
+
+		Convey("The action should be Accept when port is 85", func() {
+			So(flow, ShouldNotBeNil)
+			So(report, ShouldNotBeNil)
+			So(flow.Action, ShouldEqual, policy.Accept)
+			So(report.Action, ShouldEqual, policy.Accept)
+			So(flow, ShouldNotBeNil)
+		})
+
+		tags = policy.NewTagStore()
+		tags.AppendKeyValue("app", "web")
+		tags.AppendKeyValue(constants.PortNumberLabelString, "TCP/98")
+
+		report, flow = pu.SearchRcvRules(tags)
+
+		Convey("The action should be Accept when port is 98", func() {
+			So(flow, ShouldNotBeNil)
+			So(report, ShouldNotBeNil)
+			So(flow.Action, ShouldEqual, policy.Accept)
+			So(report.Action, ShouldEqual, policy.Accept)
+			So(flow, ShouldNotBeNil)
+		})
+
+		tags = policy.NewTagStore()
+		tags.AppendKeyValue("app", "web")
+		tags.AppendKeyValue(constants.PortNumberLabelString, "TCP/101")
+
+		report, flow = pu.SearchRcvRules(tags)
+
+		Convey("The action should be Reject when port is 101", func() {
+			So(flow, ShouldNotBeNil)
+			So(report, ShouldNotBeNil)
+			So(flow.Action, ShouldEqual, policy.Reject)
+			So(report.Action, ShouldEqual, policy.Reject)
+			So(flow, ShouldNotBeNil)
+		})
+
 	})
 }
