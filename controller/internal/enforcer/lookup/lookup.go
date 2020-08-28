f1a38066ef2a9641029c0c8c95e25cda7bed33cb
tillery-aporeto <52420493+tillery-aporeto@users.noreply.github.com>
2020-08-28 17:43:40 -0400

    Policy port range changes (#1038)
    Policy port range changes

--- controller/internal/enforcer/lookup/lookup.go
+++ controller/internal/enforcer/lookup/lookup.go
@@ -4,8 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"sort"
+	"strings"
 
+	"go.aporeto.io/trireme-lib/controller/constants"
 	"go.aporeto.io/trireme-lib/policy"
+	"go.aporeto.io/trireme-lib/utils/portspec"
 	"go.uber.org/zap"
 )
 
@@ -201,7 +204,7 @@ func (m *PolicyDB) Search(tags *policy.TagStore) (int, interface{}) {
 	for _, t := range copiedTags {
 
 		// Search for matches of t (tag id)
-		if index, action := searchInMapTable(m.equalIDMapTable[t], count, skip); index >= 0 {
+		if index, action := searchInMapTable(m.equalIDMapTable[t], nil, count, skip); index >= 0 {
 			return index, action
 		}
 
@@ -209,15 +212,26 @@ func (m *PolicyDB) Search(tags *policy.TagStore) (int, interface{}) {
 			continue
 		}
 
+		var ports *portspec.PortSpec
+		if k == constants.PortNumberLabelString {
+			// We should get range here
+			tagValue, servicePorts, err := parseTagValueRange(v)
+			if err != nil || servicePorts == nil {
+				continue
+			}
+			v = tagValue
+			ports = servicePorts
+		}
+
 		// Search for matches of k=v
-		if index, action := searchInMapTable(m.equalMapTable[k][v], count, skip); index >= 0 {
+		if index, action := searchInMapTable(m.equalMapTable[k][v], ports, count, skip); index >= 0 {
 			return index, action
 		}
 
 		// Search for matches in prefixes
 		for _, i := range m.equalPrefixes[k] {
 			if i <= len(v) {
-				if index, action := searchInMapTable(m.equalMapTable[k][v[:i]], count, skip); index >= 0 {
+				if index, action := searchInMapTable(m.equalMapTable[k][v[:i]], nil, count, skip); index >= 0 {
 					return index, action
 				}
 			}
@@ -230,7 +244,7 @@ func (m *PolicyDB) Search(tags *policy.TagStore) (int, interface{}) {
 				continue
 			}
 
-			if index, action := searchInMapTable(policies, count, skip); index >= 0 {
+			if index, action := searchInMapTable(policies, nil, count, skip); index >= 0 {
 				return index, action
 			}
 		}
@@ -243,7 +257,7 @@ func (m *PolicyDB) Search(tags *policy.TagStore) (int, interface{}) {
 	return -1, nil
 }
 
-func searchInMapTable(table []*ForwardingPolicy, count []int, skip []bool) (int, interface{}) {
+func searchInMapTable(table []*ForwardingPolicy, ports *portspec.PortSpec, count []int, skip []bool) (int, interface{}) {
 	for _, policy := range table {
 
 		// Skip the policy if we have marked it
@@ -251,8 +265,17 @@ func searchInMapTable(table []*ForwardingPolicy, count []int, skip []bool) (int,
 			continue
 		}
 
-		// Since a policy is hit, the count of remaining tags is reduced by one
-		count[policy.index]++
+		if ports != nil {
+			for _, tag := range policy.tags {
+				if tag.PortRange != nil && tag.PortRange.Intersects(ports) {
+					count[policy.index]++
+					break
+				}
+			}
+		} else {
+			// Since a policy is hit, the count of remaining tags is reduced by one
+			count[policy.index]++
+		}
 
 		// If all tags of the policy have been hit, there is a match
 		if count[policy.index] == policy.count {
@@ -302,4 +325,17 @@ func (m *PolicyDB) PrintPolicyDB() {
 		}
 	}
 
+}
+
+func parseTagValueRange(value string) (string, *portspec.PortSpec, error) {
+	index := strings.Index(value, "/")
+	if index == -1 {
+		// means there was no range
+		return value, nil, nil
+	}
+	rangeSpec, err := portspec.NewPortSpecFromString(value[index+1:], nil)
+	if err != nil {
+		return "", nil, err
+	}
+	return value[:index], rangeSpec, nil
 }
